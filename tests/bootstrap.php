0757f01b03dccd9f9b53071915673b312e7f597f
Roman3349 <ondracek.roman@centrum.cz>
2017-04-24 21:07:48 +0200

    Add skeleton for Nette Tester
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+Tester\Environment::setup();
+
+$configurator = new Nette\Configurator;
+$configurator->setDebugMode(FALSE);
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->createRobotLoader()->addDirectory(__DIR__ . '/../app')->register();
+$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
+return $configurator->createContainer();
+
