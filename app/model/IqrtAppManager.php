9f91ebda6365c7faca100467fefb99a06a89fa4d
Roman Ondráček <ondracek.roman@centrum.cz>
2017-05-05 22:20:04 +0200

    Add sending raw IQRF packets via IQRF App
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

---
+++ app/model/IqrtAppManager.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Model;
+
+use Nette;
+
+class IqrfAppManager {
+
+	use Nette\SmartObject;
+
+	/**
+	 * @var bool
+	 * @inject
+	 */
+	private $sudo;
+
+	/**
+	 * Constructor
+	 * @param bool $sudo Sudo required
+	 */
+	public function __construct($sudo) {
+		//$this->sudo = $sudo;
+		$this->sudo = false;
+	}
+
+	/**
+	 * Send RAW IQRF packet
+	 * @param string $packet RAW IQRF packet
+	 */
+	public function sendRaw($packet) {
+		$cmd = $this->sudo ? 'sudo ' : '';
+		$cmd .= 'iqrfapp raw ' . $packet;
+		return shell_exec($cmd);
+	}
+
+}
---
+++ app/presenters/IqrfAppPresenter.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Presenters;
+
+use App\Forms;
+use App\Model\IqrfAppManager;
+
+/**
+ * Service presenter
+ */
+class IqrfAppPresenter extends BasePresenter {
+
+	/**
+	 * @var Forms\IqrfAppSendRawFormFactory
+	 * @inject
+	 */
+	public $iqrfAppSendRawFactory;
+
+	/**
+	 * @var IqrfAppManager
+	 * @inject
+	 */
+	private $iqrfAppManager;
+
+	/**
+	 * @param IqrfAppManager $iqrfAppManager
+	 */
+	public function __construct(IqrfAppManager $iqrfAppManager) {
+		$this->iqrfAppManager = $iqrfAppManager;
+	}
+
+	public function renderDefault() {
+		if (!$this->user->isLoggedIn()) {
+			$this->redirect('Sign:in');
+		}
+	}
+
+	public function handleShowResponse($response) {
+		$this->template->response = $response;
+		$this->redrawControl('responseChange');
+	}
+
+	protected function createComponentIqrfAppSendRawForm() {
+		if (!$this->user->isLoggedIn()) {
+			$this->redirect('Sign:in');
+		}
+		return $this->iqrfAppSendRawFactory->create($this);
+	}
+
+}
