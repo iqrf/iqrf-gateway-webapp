2e6717bd0672fbcbac6ca5c7ecc883c084d2b0df
Roman Ondráček <ondracek.roman@centrum.cz>
2017-08-12 17:00:44 +0200

    Refactor selecting available interface in IQRF interface configurator
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- www/js/iqrfApp.js
+++ www/js/iqrfApp.js
@@ -1,3 +1,6 @@
 $(".btn-packet").click(function () {
 	$("#frm-iqrfAppSendRawForm-packet").val($(this).data("packet"));
 });
+$(".btn-port").click(function () {
+	$("#frm-configIqrfForm-IqrfInterface").val($(this).data("port"));
+});
