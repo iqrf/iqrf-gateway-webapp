2948cd60c7fa506f135a3eaa762d08fff2275a87
Roman Ondráček <roman.ondracek@iqrf.com>
2017-11-17 19:23:46 +0100

    Fix path to teplates for error pages
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

--- app/presenters/Error4xxPresenter.php
+++ app/presenters/Error4xxPresenter.php
@@ -43,8 +43,10 @@ class Error4xxPresenter extends BasePresenter {
 	 */
 	public function renderDefault(BadRequestException $exception) {
 		// load template 403.latte or 404.latte or ... 4xx.latte
-		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
-		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+		$directory = __DIR__ . '/../templates/Error/';
+		$file = $directory . $exception->getCode() . '.latte';
+		$template = is_file($file) ? $file : $directory . '4xx.latte';
+		$this->template->setFile($template);
 	}
 
 }
