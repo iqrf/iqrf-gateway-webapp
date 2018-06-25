9ad4e53d8a4bdb194b41981f58c2510b7a180cd1
Roman Ondráček <roman.ondracek@iqrf.com>
2018-06-25 20:27:37 +0200

    Enable tests for IQRF interface manager
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

--- tests/ConfigModule/model/IqrfManagerTest.php
+++ tests/ConfigModule/model/IqrfManagerTest.php
@@ -5,7 +5,6 @@
  * @covers App\ConfigModule\Model\IqrfManager
  * @phpVersion >= 7.0
  * @testCase
- * @skip
  */
 declare(strict_types=1);
 
@@ -13,9 +12,7 @@ namespace Test\ConfigModule\Model;
 
 use App\Model\CommandManager;
 use App\ConfigModule\Model\IqrfManager;
-use App\Model\JsonFileManager;
 use Nette\DI\Container;
-use Nette\Utils\ArrayHash;
 use Tester\Assert;
 use Tester\TestCase;
 
@@ -34,31 +31,6 @@ class IqrfManagerTest extends TestCase {
 	private $commandManager;
 
 	/**
-	 * @var JsonFileManager JSON file manager
-	 */
-	private $fileManager;
-
-	/**
-	 * @var JsonFileManager JSON file manager
-	 */
-	private $fileManagerTemp;
-
-	/**
-	 * @var string File name (without .json)
-	 */
-	private $fileName = 'IqrfInterface';
-
-	/**
-	 * @var string Directory with configuration files
-	 */
-	private $path = __DIR__ . '/../../configuration/';
-
-	/**
-	 * @var string Testing directory with configuration files
-	 */
-	private $pathTest = __DIR__ . '/../../configuration-test/';
-
-	/**
 	 * Constructor
 	 * @param Container $container Nette Tester Container
 	 */
@@ -71,41 +43,6 @@ class IqrfManagerTest extends TestCase {
 	 */
 	public function setUp() {
 		$this->commandManager = new CommandManager(false);
-		$this->fileManager = new JsonFileManager($this->path);
-		$this->fileManagerTemp = new JsonFileManager($this->pathTest);
-	}
-
-	/**
-	 * @test
-	 * Test function to load main configuration of daemon
-	 */
-	public function testLoad() {
-		$manager = new IqrfManager($this->commandManager, $this->fileManager);
-		$expected = $this->fileManager->read($this->fileName);
-		Assert::equal($expected, $manager->load());
-	}
-
-	/**
-	 * @test
-	 * Test function to save main configuration of daemon
-	 */
-	public function testSave() {
-		$manager = new IqrfManager($this->commandManager, $this->fileManagerTemp);
-		$array = [
-			'IqrfInterface' => 'COM6',
-			'DpaHandlerTimeout' => 500,
-			'CommunicationMode' => 'LP',
-			'enableGpioPin' => null,
-			'spiCe0GpioPin' => null,
-			'spiMisoGpioPin' => null,
-			'spiMosiGpioPin' => null,
-			'spiClkGpioPin' => null,
-		];
-		$expected = $this->fileManager->read($this->fileName);
-		$this->fileManagerTemp->write($this->fileName, $expected);
-		$expected['CommunicationMode'] = 'LP';
-		$manager->save(ArrayHash::from($array));
-		Assert::equal($expected, $this->fileManagerTemp->read($this->fileName));
 	}
 
 	/**
@@ -116,7 +53,7 @@ class IqrfManagerTest extends TestCase {
 		$commandManager = \Mockery::mock(CommandManager::class);
 		$output = '/dev/ttyACM0' . PHP_EOL . '/dev/ttyACM1';
 		$commandManager->shouldReceive('send')->with('ls /dev/ttyACM* | awk \'{ print $0 }\'', true)->andReturn($output);
-		$manager = new IqrfManager($commandManager, $this->fileManager);
+		$manager = new IqrfManager($commandManager);
 		$expected = ['/dev/ttyACM0', '/dev/ttyACM1'];
 		Assert::same($expected, $manager->getCdcInterfaces());
 	}
@@ -129,7 +66,7 @@ class IqrfManagerTest extends TestCase {
 		$commandManager = \Mockery::mock(CommandManager::class);
 		$output = '/dev/spidev0.0' . PHP_EOL . '/dev/spidev0.1' . PHP_EOL . '/dev/spidev1.0' . PHP_EOL . '/dev/spidev1.1';
 		$commandManager->shouldReceive('send')->with('ls /dev/spidev* | awk \'{ print $0 }\'', true)->andReturn($output);
-		$manager = new IqrfManager($commandManager, $this->fileManager);
+		$manager = new IqrfManager($commandManager);
 		$expected = ['/dev/spidev0.0', '/dev/spidev0.1', '/dev/spidev1.0', '/dev/spidev1.1'];
 		Assert::same($expected, $manager->getSpiInterfaces());
 	}
