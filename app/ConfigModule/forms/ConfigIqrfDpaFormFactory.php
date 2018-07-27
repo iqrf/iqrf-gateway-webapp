cba3aa66e7d25b69780be499056dfaca856b8a68
Roman Ondráček <roman.ondracek@iqrf.com>
2018-07-27 22:27:31 +0200

    Add missing configuration parameters to IQRF DPA configuration tool
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

--- app/ConfigModule/forms/ConfigIqrfDpaFormFactory.php
+++ app/ConfigModule/forms/ConfigIqrfDpaFormFactory.php
@@ -60,12 +60,21 @@ class ConfigIqrfDpaFormFactory {
 		$form = $this->factory->create();
 		$form->setTranslator($form->getTranslator()->domain('config.iqrfDpa.form'));
 		$communicationModes = ['STD' => 'CommunicationModes.STD', 'LP' => 'CommunicationModes.LP'];
+		$responseTimes = ['k40Ms', 'k360Ms', 'k680Ms', 'k1320Ms', 'k2600Ms',
+			'k5160Ms', 'k10280Ms', 'k20620Ms',];
+		foreach ($responseTimes as $key => $time) {
+			unset($responseTimes[$key]);
+			$responseTimes[$time] = 'ResponseTimes.' . $time;
+		}
 		$this->manager->setComponent('iqrf::IqrfDpa');
 		$this->manager->setFileName($this->manager->getInstanceFiles()[0]);
 		$form->addText('instance', 'instance')->setRequired('messages.instance');
 		$form->addInteger('DpaHandlerTimeout', 'DpaHandlerTimeout')->setRequired('messages.DpaHandlerTimeout')
 				->addRule(Form::MIN, 'messages.DpaHandlerTimeout-rule', 0);
 		$form->addSelect('CommunicationMode', 'CommunicationMode', $communicationModes);
+		$form->addInteger('BondedNodes', 'BondedNodes');
+		$form->addInteger('DiscoveredNodes', 'DiscoveredNodes');
+		$form->addSelect('ResponseTime', 'ResponseTime', $responseTimes);
 		$form->addSubmit('save', 'Save');
 		$form->setDefaults($this->manager->load());
 		$form->addProtection('core.errors.form-timeout');
