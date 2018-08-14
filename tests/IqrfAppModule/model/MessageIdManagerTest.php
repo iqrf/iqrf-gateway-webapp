4a20c7aa34ab5617f469f62ec23282099461c87c
Roman Ondráček <roman.ondracek@iqrf.com>
2018-08-14 16:47:31 +0200

    Add a test for a generation of message ID
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

---
+++ tests/IqrfAppModule/model/MessageIdManagerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * TEST: App\IqrfAppModule\Model\MessageIdManager
+ * @covers App\IqrfAppModule\Model\MessageIdManager
+ * @phpVersion >= 7.0
+ * @testCase
+ */
+declare(strict_types = 1);
+
+namespace Test\IqrfAppModule\Model;
+
+use App\IqrfAppModule\Model\MessageIdManager;
+use Nette\DI\Container;
+use Tester\Assert;
+use Tester\TestCase;
+
+$container = require __DIR__ . '/../../bootstrap.php';
+
+/**
+ * Tests for message ID generator
+ */
+class MessageIdManagerTest extends TestCase {
+
+	/**
+	 * @var Container Nette Tester Container
+	 */
+	private $container;
+
+	/**
+	 * Constructor
+	 * @param Container $container Nette Tester Container
+	 */
+	public function __construct(Container $container) {
+		$this->container = $container;
+	}
+	
+	/**
+	 * Test function to generate message ID
+	 */
+	public function testGenerate() {
+		$expected = strval((new \DateTime())->getTimestamp());
+		$manager = new MessageIdManager();
+		Assert::same($expected, $manager->generate());
+	}
+	
+}
+
+
+$test = new MessageIdManagerTest($container);
+$test->run();
