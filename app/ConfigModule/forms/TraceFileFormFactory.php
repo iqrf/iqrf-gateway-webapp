e2a56831f866ed32db76e1ce7063d4fe52d47f0a
Roman Ondráček <roman.ondracek@iqrf.com>
2018-12-09 20:10:55 +0100

    Revert "Remove a configuration tool for Tracer"
    This reverts commit 969b808874ba5b85cd9de6d778378e34be2a2e75.
    
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

---
+++ app/ConfigModule/forms/TraceFileFormFactory.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2018 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types=1);
+
+namespace App\ConfigModule\Forms;
+
+use App\ConfigModule\Presenters\TracerPresenter;
+use Nette\Application\UI\Form;
+use Nette\SmartObject;
+use Nette\Utils\Strings;
+
+class TraceFileFormFactory extends GenericConfigFormFactory {
+
+	use SmartObject;
+
+	/**
+	 * Create Tracer configuration form
+	 * @param TracerPresenter $presenter
+	 * @return Form Tracer configuration form
+	 */
+	public function create(TracerPresenter $presenter): Form {
+		$form = $this->factory->create();
+		$form->setTranslator($form->getTranslator()->domain('config.tracer.form'));
+		$this->manager->setComponent('shape::TraceFileService');
+		$this->manager->setFileName($this->manager->getInstanceFiles()[0]);
+		$defaults = $this->manager->load();
+		$items = ['err' => 'VerbosityLevels.Error', 'war' => 'VerbosityLevels.Warning',
+			'inf' => 'VerbosityLevels.Info', 'dbg' => 'VerbosityLevels.Debug'];
+		$form->addText('instance', 'instance');
+		$form->addText('path', 'path');
+		$form->addText('filename', 'filename');
+		$form->addInteger('maxSizeMB', 'maxSizeMB');
+		$form->addCheckbox('timestampFiles', 'timestampFiles');
+		$verbosityLevels = $form->addContainer('VerbosityLevels');
+		foreach ($defaults['VerbosityLevels'] as $id => $verbosityLevel) {
+			$container = $verbosityLevels->addContainer($id);
+			$container->addInteger('channel', 'channel');
+			$container->addSelect('level', 'level', $items)
+					->setDefaultValue(Strings::lower($verbosityLevel['level']));
+			unset($defaults['VerbosityLevels'][$id]['level']);
+		}
+		$form->addSubmit('save', 'Save');
+		$form->setDefaults($defaults);
+		$form->addProtection('core.errors.form-timeout');
+		$form->onSuccess[] = function (Form $form, $values) use ($presenter) {
+			$this->manager->save($values);
+			$presenter->redirect('Homepage:default');
+		};
+		return $form;
+	}
+
+}
---
+++ app/ConfigModule/presenters/TracerPresenter.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2018 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types=1);
+
+namespace App\ConfigModule\Presenters;
+
+use App\ConfigModule\Forms\TraceFileFormFactory;
+use App\ConfigModule\Models\GenericManager;
+
+class TracerPresenter extends GenericPresenter {
+
+	/**
+	 * @var TraceFileFormFactory Daemon's tracer configuration form factory
+	 * @inject
+	 */
+	public $formFactory;
+
+	/**
+	 * Constructor
+	 * @param GenericManager $genericManager Generic configuration manager
+	 */
+	public function __construct(GenericManager $genericManager) {
+		$components = ['shape::TraceFileService'];
+		parent::__construct($components, $genericManager);
+	}
+
+	/**
+	 * Edit UDP interface
+	 * @param int $id ID of UDP interface
+	 */
+	public function renderEdit(int $id): void {
+		$this->template->id = $id;
+	}
+
+	/**
+	 * Delete trace file service
+	 * @param int $id ID of trace file service
+	 * @throws JsonException
+	 */
+	public function actionDelete(int $id): void {
+		$this->configManager->setComponent('shape::TraceFileService');
+		$this->configManager->delete($id);
+		$this->redirect('TracerFile:default');
+		$this->setView('default');
+	}
+
+	/**
+	 * Create Tracer form
+	 * @return Form Tracer form
+	 */
+	protected function createComponentConfigTracerForm() {
+		$this->onlyForAdmins();
+		return $this->formFactory->create($this);
+	}
+
+}
