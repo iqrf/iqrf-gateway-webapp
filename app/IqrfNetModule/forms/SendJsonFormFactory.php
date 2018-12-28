4344dc278184a2a36fab2ce37f141486c5cde29a
Roman Ondráček <roman.ondracek@iqrf.com>
2018-12-28 21:46:46 +0100

    Add form for sending a raw JSON DPA requests
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

---
+++ app/IqrfNetModule/forms/SendJsonFormFactory.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2018 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types = 1);
+
+namespace App\IqrfNetModule\Forms;
+
+use App\CoreModule\Forms\FormFactory;
+use App\IqrfNetModule\Exceptions\DpaErrorException;
+use App\IqrfNetModule\Exceptions\EmptyResponseException;
+use App\IqrfNetModule\Models\WebSocketClient;
+use App\IqrfNetModule\Presenters\SendJsonPresenter;
+use App\IqrfNetModule\Requests\DpaRequest;
+use Nette\Forms\Form;
+use Nette\SmartObject;
+use Nette\Utils\Json;
+use Nette\Utils\JsonException;
+
+/**
+ * Send raw JSON request form factory
+ */
+class SendJsonFormFactory {
+
+	use SmartObject;
+
+	/**
+	 * @var FormFactory Generic form factory
+	 */
+	private $factory;
+
+	/**
+	 * @var SendJsonPresenter IQRF Send raw JSON DPA request presenter
+	 */
+	private $presenter;
+
+	/**
+	 * @var DpaRequest JSON DPA request
+	 */
+	private $request;
+
+	/**
+	 * @var WebSocketClient WebSocket client
+	 */
+	private $wsClient;
+
+	/**
+	 * Constructor
+	 * @param FormFactory $factory Generic form factory
+	 * @param DpaRequest $request JSON DPA request
+	 * @param WebSocketClient $wsClient WebSocket client
+	 */
+	public function __construct(FormFactory $factory, DpaRequest $request, WebSocketClient $wsClient) {
+		$this->factory = $factory;
+		$this->request = $request;
+		$this->wsClient = $wsClient;
+	}
+
+	/**
+	 * Create IQRF App send JSON request form
+	 * @param SendJsonPresenter $presenter IQRF Send JSON request presenter
+	 * @return Form IQRF App send RAW packet form
+	 */
+	public function create(SendJsonPresenter $presenter): Form {
+		$this->presenter = $presenter;
+		$form = $this->factory->create();
+		$form->setTranslator($form->getTranslator()->domain('iqrfnet.send-json'));
+		$form->addTextArea('json', 'json')->setRequired('messages.json');
+		$form->addSubmit('send', 'send');
+		$form->addProtection('core.errors.form-timeout');
+		$form->onSuccess[] = [$this, 'onSuccess'];
+		return $form;
+	}
+
+	/**
+	 * Send raw DPA packet
+	 * @param Form $form IQRF App send RAW packet form
+	 * @throws JsonException
+	 */
+	public function onSuccess(Form $form): void {
+		$values = $form->getValues();
+		$array = Json::decode($values['json'], Json::FORCE_ARRAY);
+		try {
+			$this->request->setRequest($array);
+			$response = $this->wsClient->sendSync($this->request);
+			$this->presenter->handleShowResponse($response);
+		} catch (EmptyResponseException | DpaErrorException $e) {
+			$message = 'No response from IQRF Gateway Daemon.';
+			$form->addError($message);
+			$this->presenter->flashMessage($message, 'danger');
+		}
+	}
+
+}
---
+++ app/IqrfNetModule/presenters/SendJsonPresenter.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2018 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types = 1);
+
+namespace App\IqrfNetModule\Presenters;
+
+use App\CoreModule\Presenters\ProtectedPresenter;
+use App\IqrfNetModule\Forms\SendJsonFormFactory;
+use Nette\Forms\Form;
+use Nette\Utils\Json;
+
+/**
+ * Send raw JSON DPA request presenter
+ */
+class SendJsonPresenter extends ProtectedPresenter {
+
+	/**
+	 * @var SendJsonFormFactory Send raw JSON DPA request form
+	 * @inject
+	 */
+	public $sendJsonFactory;
+
+	/**
+	 * AJAX handler for showing DPA request and response
+	 * @param array $data DPA request and response
+	 */
+	public function handleShowResponse(array $data): void {
+		foreach ($data as &$json) {
+			$json = Json::encode($json, Json::PRETTY);
+		}
+		$this->template->json = $data;
+		$this->redrawControl('responseChange');
+	}
+
+	/**
+	 * Create send raw JSON DPA request form
+	 * @return Form Send raw JSON DPA request form
+	 */
+	protected function createComponentSendJsonForm(): Form {
+		return $this->sendJsonFactory->create($this);
+	}
+
+}
