094459e3af0ba4de3898f827fa515fcadebbcdc5
Roman Ondráček <roman.ondracek@iqrf.com>
2019-01-01 00:07:11 +0100

    Add prototype of IQRF Gateway Updater
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

---
+++ app/GatewayModule/models/UpdaterManager.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2018 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types = 1);
+
+namespace App\GatewayModule\Models;
+
+use Nette\SmartObject;
+use Symfony\Component\Process\Process;
+
+/**
+ * Tool for updating packages of IQRF Gateways
+ */
+class UpdaterManager {
+
+	use SmartObject;
+
+	/**
+	 * @var bool Is sudo required?
+	 */
+	private $sudo;
+
+	/**
+	 * Constructor
+	 * @param bool $sudo Is sudo required?
+	 */
+	public function __construct(bool $sudo) {
+		$this->sudo = $sudo;
+	}
+
+	/**
+	 * Update list of packages
+	 * @param callable $callback Callback
+	 */
+	public function update(callable $callback): void {
+		$this->runCommand('apt-get update', $callback);
+	}
+
+	/**
+	 * Upgrade packages
+	 * @param callable $callback Callback
+	 */
+	public function upgrade(callable $callback): void {
+		$this->runCommand('apt-get upgrade -y', $callback);
+	}
+
+	/**
+	 * Run command in shell
+	 * @param string $cmd Shell command
+	 * @param callable $callback Callback
+	 */
+	private function runCommand(string $cmd, callable $callback): void {
+		$command = ($this->sudo ? 'sudo ' : '') . $cmd;
+		$process = Process::fromShellCommandline($command);
+		$process->setTimeout(36000);
+		$process->start($callback);
+		$process->wait();
+	}
+
+}
---
+++ app/GatewayModule/presenters/UpdaterPresenter.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2018 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types = 1);
+
+namespace App\GatewayModule\Presenters;
+
+use App\CoreModule\Presenters\ProtectedPresenter;
+use App\GatewayModule\Models\UpdaterManager;
+
+/**
+ * IQRF Gateway updater presenter
+ */
+class UpdaterPresenter extends ProtectedPresenter {
+
+	/**
+	 * @var UpdaterManager Updater manager
+	 */
+	private $manager;
+
+	/**
+	 * Constructor
+	 * @param UpdaterManager $manager Updater manager
+	 */
+	public function __construct(UpdaterManager $manager) {
+		$this->manager = $manager;
+		parent::__construct();
+	}
+
+	/**
+	 * Handle updating package's cache
+	 */
+	public function handleUpdate(): void {
+		$this->manager->update([$this, 'showCommandOutput']);
+	}
+
+	/**
+	 * Handle updating packages
+	 */
+	public function handleUpgrade(): void {
+		$this->manager->upgrade([$this, 'showCommandOutput']);
+	}
+
+	/**
+	 * Shows shell command output
+	 * @param string $type Output type
+	 * @param null|string $buffer Output buffer
+	 */
+	public function showCommandOutput(string $type, ?string $buffer): void {
+		if (isset($this->template->outputBuffer)) {
+			$this->template->outputBuffer .= $buffer;
+		} else {
+			$this->template->outputBuffer = $buffer;
+		}
+		$this->redrawControl('outputChange');
+	}
+}
