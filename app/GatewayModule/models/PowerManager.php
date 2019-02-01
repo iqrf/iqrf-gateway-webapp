bf2be24bf649ad1e8136a7d470367a55578f2d75
Roman Ondráček <roman.ondracek@iqrf.com>
2019-02-01 14:30:44 +0100

    Gateway: add actions for powering off and rebooting IQRF Gateway
    Signed-off-by: Roman Ondráček <roman.ondracek@iqrf.com>

---
+++ app/GatewayModule/models/PowerManager.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2018 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types = 1);
+
+namespace App\GatewayModule\Models;
+
+use App\CoreModule\Models\CommandManager;
+use Nette\SmartObject;
+
+/**
+ * Tool for powering off and rebooting IQRF Gateway
+ */
+class PowerManager {
+
+	use SmartObject;
+
+	/**
+	 * @var CommandManager Command manager
+	 */
+	private $commandManager;
+
+	/**
+	 * Constructor
+	 * @param CommandManager $commandManager Command manager
+	 */
+	public function __construct(CommandManager $commandManager) {
+		$this->commandManager = $commandManager;
+	}
+
+	/**
+	 * Power off IQRF Gateway
+	 */
+	public function powerOff(): void {
+		$this->commandManager->send('poweroff', true);
+	}
+
+	/**
+	 * Reboot IQRF Gateway
+	 */
+	public function reboot(): void {
+		$this->commandManager->send('reboot', true);
+	}
+
+}
