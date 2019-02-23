53c11a30a812e4356bcab365123fcb789f5c8ba1
Roman Ondráček <ondracek.roman@centrum.cz>
2019-02-23 21:51:20 +0100

    Config: fix JS for the scheduler configuration tool
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- www/js/config/scheduler.js
+++ www/js/config/scheduler.js
@@ -28,44 +28,113 @@ function parseCron() {
 	let time = document.getElementById('frm-configSchedulerForm-timeSpec-cronTime');
 	let value = time.value;
 	let len = value.split(' ').length;
-	let aliases = new Map();
-	aliases.set('@reboot', '');
-	aliases.set('@yearly', '0 0 0 0 1 1 *');
-	aliases.set('@annually', '0 0 0 0 1 1 *');
-	aliases.set('@monthly', '0 0 0 0 1 * *');
-	aliases.set('@weekly', '0 0 0 * * * 0');
-	aliases.set('@daily', '0 0 0 * * * *');
-	aliases.set('@hourly', '0 0 * * * * *');
-	aliases.set('@minutely', '0 * * * * * *');
 	if (len === 1) {
-		value = aliases.get(value);
+		value = getCronAlias(value);
 		if (value === undefined) {
+			if (cron !== null) {
+				cron.style.visibility = 'hidden';
+			}
 			return;
+		} else {
+			text = cronstrue.toString(value);
+			return printCronDescription(text);
 		}
-		len = value.split(' ').length;
 	}
-	if (len > 5 && len < 8) {
+	if (len > 4 && len < 8) {
 		try {
 			text = cronstrue.toString(value);
-			if (cron === null) {
-				let label = document.createElement('span');
-				label.id = 'frm-configSchedulerForm-cron';
-				label.innerText = text;
-				label.className = 'label label-info';
-				time.insertAdjacentHTML('beforebegin', label.outerHTML);
-			} else {
-				cron.innerText = text;
-			}
+			printCronDescription(text);
 		} catch (e) {
+			if (cron !== null) {
+				cron.style.visibility = 'hidden';
+			}
 			console.error(e);
 		}
+	} else if (cron !== null) {
+		cron.style.visibility = 'hidden';
 	}
 }
 
-let time = document.getElementById('frm-configSchedulerForm-timeSpec-cronTime');
-if (time !== null) {
+/**
+ * Returns CRON expresion from an alias
+ * @param input CRON alias
+ * @returns string CRON expresion
+ */
+function getCronAlias(input) {
+	let aliases = new Map();
+	aliases.set('@reboot', '');
+	aliases.set('@yearly', '0 0 0 1 1 * *');
+	aliases.set('@annually', '0 0 0 1 1 * *');
+	aliases.set('@monthly', '0 0 0 1 * * *');
+	aliases.set('@weekly', '0 0 0 * * 0 *');
+	aliases.set('@daily', '0 0 0 * * * *');
+	aliases.set('@hourly', '0 0 * * * * *');
+	aliases.set('@minutely', '0 * * * * * *');
+	return aliases.get(input);
+}
+
+/**
+ * Prints CRON expresion description
+ * @param expresion CRON expresion
+ */
+function printCronDescription(expresion) {
+	let cron = document.getElementById('frm-configSchedulerForm-cron');
+	if (cron === null) {
+		let time = document.getElementById('frm-configSchedulerForm-timeSpec-cronTime');
+		let label = document.createElement('span');
+		label.id = 'frm-configSchedulerForm-cron';
+		label.innerText = expresion;
+		label.className = 'label label-info';
+		time.insertAdjacentHTML('beforebegin', label.outerHTML);
+	} else {
+		cron.innerText = expresion;
+		cron.style.visibility = 'visible';
+	}
+}
+
+/**
+ * Disables other time specification
+ * @param event Event
+ */
+function disableOtherTimeScpec(event) {
+	let cronTime = document.getElementById('frm-configSchedulerForm-timeSpec-cronTime');
+	let exactTime = document.getElementById('frm-configSchedulerForm-timeSpec-exactTime');
+	let periodic = document.getElementById('frm-configSchedulerForm-timeSpec-periodic');
+	let period = document.getElementById('frm-configSchedulerForm-timeSpec-period');
+	let startTime = document.getElementById('frm-configSchedulerForm-timeSpec-startTime');
+	let target = event.currentTarget;
+	let disabled = target.checked;
+	if (cronTime !== null) {
+		cronTime.disabled = disabled;
+	}
+	if (exactTime !== null && target === periodic) {
+		exactTime.disabled = disabled;
+	}
+	if (periodic !== null && target === exactTime) {
+		periodic.disabled = disabled;
+	}
+	if (period !== null) {
+		period.disabled = (target === periodic) ? !disabled : true;
+
+	}
+	if (startTime !== null) {
+		startTime.disabled = (target === exactTime) ? !disabled : true;
+	}
+}
+
+let cronTime = document.getElementById('frm-configSchedulerForm-timeSpec-cronTime');
+let exactTime = document.getElementById('frm-configSchedulerForm-timeSpec-exactTime');
+let periodic = document.getElementById('frm-configSchedulerForm-timeSpec-periodic');
+
+if (exactTime !== null) {
+	exactTime.addEventListener('change', disableOtherTimeScpec);
+}
+if (periodic !== null) {
+	periodic.addEventListener('change', disableOtherTimeScpec);
+}
+if (cronTime !== null) {
 	parseCron();
-	time.addEventListener('keyup', function () {
+	cronTime.addEventListener('keyup', function () {
 		parseCron();
 	});
 }
