0caaf4f45d950cc09ca8e9478a4591b255becb36
Roman Ondráček <ondracek.roman@centrum.cz>
2019-05-14 11:50:53 +0200

    Docker: add support for armel, armhf, arm64, i386 and ppc64le
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

---
+++ docker/latest/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+TAG="latest"
+TAGS=""
+ARCHS=("amd64" "armel" "armhf" "arm64" "i386" "ppc64le")
+REPO="iqrftech/iqrf-gateway-webapp"
+
+for ARCH in "${ARCHS[@]}"
+do
+   docker build --no-cache -f "${ARCH}.Dockerfile" -t "${REPO}:${TAG}-${ARCH}" .
+   docker push "${REPO}:${TAG}-${ARCH}"
+   TAGS="${TAGS} ${REPO}:${TAG}-${ARCH}"
+done
+
+export DOCKER_CLI_EXPERIMENTAL="enabled"
+docker manifest create "${REPO}:${TAG}" ${TAGS}
+docker manifest annotate "${REPO}:${TAG}" "${REPO}:${TAG}-armel" --os=linux --arch=arm --variant=v6
+docker manifest push "$REPO":"$TAG"
---
+++ docker/stable/ppc64le.Dockerfile
@@ -0,0 +1,16 @@
+FROM ppc64le/composer:latest
+
+MAINTAINER Roman Ondráček <roman.ondracek@iqrf.com>
+LABEL maintainer="roman.ondracek@iqrf.com"
+
+WORKDIR /var/www/iqrf-gateway-webapp
+
+RUN composer create-project iqrf/iqrf-gateway-webapp .
+RUN sed -i 's/sudo\:\ true/sudo\:\ false/g' app/config/config.neon
+RUN sed -i "s/initDaemon: 'systemd'/initDaemon: 'docker'/g" app/config/config.neon
+RUN chmod 777 log/ \
+ && chmod 777 temp/
+
+CMD [ "php", "-S", "[::]:8080", "-t", "/var/www/iqrf-gateway-webapp/www/" ]
+
+EXPOSE 8080
