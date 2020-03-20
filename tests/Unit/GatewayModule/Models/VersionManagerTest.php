4f7fde9c46a19e514b549c89c64465e05ccf93c3
Roman Ondráček <ondracek.roman@centrum.cz>
2020-03-20 22:21:44 +0100

    Prepare for version 2.0.0-rc7
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Unit/GatewayModule/Models/VersionManagerTest.php
+++ tests/Unit/GatewayModule/Models/VersionManagerTest.php
@@ -73,7 +73,7 @@ class VersionManagerTest extends WebSocketTestCase {
 	/**
 	 * IQRF Gateway Webapp's version
 	 */
-	private const WEBAPP_VERSION = 'v2.0.0-rc6';
+	private const WEBAPP_VERSION = 'v2.0.0-rc7';
 
 	/**
 	 * Sets up the test environment
