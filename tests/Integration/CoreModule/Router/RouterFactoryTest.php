3c323cff7ae0f480c3dbb7fe2edce9588c5c886e
Roman Ondráček <ondracek.roman@centrum.cz>
2020-04-17 11:27:49 +0200

    Tests: fix coding style
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Integration/CoreModule/Router/RouterFactoryTest.php
+++ tests/Integration/CoreModule/Router/RouterFactoryTest.php
@@ -28,32 +28,41 @@ class RouterFactoryTest extends TestCase {
 	 * @var mixed[] Routes
 	 */
 	private $expected = [
-		['Cloud:' => [
-			'[<lang [a-z]{2}>/]cloud/<presenter>/<action>[/<id>]',
-		],],
-		['Config:' => [
-			'[<lang [a-z]{2}>/]config/scheduler/add/<type>',
-			'[<lang [a-z]{2}>/]config/<presenter>/<action>[/<id>]',
-		],],
-		['Gateway:' => [
-			'[<lang [a-z]{2}>/]gateway/<presenter>/<action>',
-		],],
-		['Install:' => [
-			'[<lang [a-z]{2}>/]install/<presenter>/<action>',
-		],],
-		['IqrfNet:' => [
-			'[<lang [a-z]{2}>/]iqrfnet/enumeration/<address>',
-			'[<lang [a-z]{2}>/]iqrfnet/tr-config/<address>',
-			'[<lang [a-z]{2}>/]iqrfnet/<presenter>/<action>',
-		],],
-		['Network:' => [
-			'[<lang [a-z]{2}>/]network/<presenter>/<action>[/<uuid>]',
-		],],
-		['Service:' => [
-			'[<lang [a-z]{2}>/]service/<presenter>/<action>',
-		],],
-		['Core:' =>
-			['[<lang [a-z]{2}>/]<presenter>/<action>[/<id>]'],
+		[
+			'Cloud:' => [
+				'[<lang [a-z]{2}>/]cloud/<presenter>/<action>[/<id>]',
+			],
+		], [
+			'Config:' => [
+				'[<lang [a-z]{2}>/]config/scheduler/add/<type>',
+				'[<lang [a-z]{2}>/]config/<presenter>/<action>[/<id>]',
+			],
+		], [
+			'Gateway:' => [
+				'[<lang [a-z]{2}>/]gateway/<presenter>/<action>',
+			],
+		], [
+			'Install:' => [
+				'[<lang [a-z]{2}>/]install/<presenter>/<action>',
+			],
+		], [
+			'IqrfNet:' => [
+				'[<lang [a-z]{2}>/]iqrfnet/enumeration/<address>',
+				'[<lang [a-z]{2}>/]iqrfnet/tr-config/<address>',
+				'[<lang [a-z]{2}>/]iqrfnet/<presenter>/<action>',
+			],
+		], [
+			'Network:' => [
+				'[<lang [a-z]{2}>/]network/<presenter>/<action>[/<uuid>]',
+			],
+		], [
+			'Service:' => [
+				'[<lang [a-z]{2}>/]service/<presenter>/<action>',
+			],
+		], [
+			'Core:' => [
+				'[<lang [a-z]{2}>/]<presenter>/<action>[/<id>]',
+			],
 		],
 	];
 
