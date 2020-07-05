f00c1ef06009a19a65b3a029220f353f69a49d57
Roman Ondráček <ondracek.roman@centrum.cz>
2020-07-05 13:21:52 +0200

    IQRF net: use Doctrine ORM for IQRF OS patches
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

---
+++ app/ConsoleModule/Commands/IqrfOsPatchImportCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Copyright 2017 MICRORISC s.r.o.
+ * Copyright 2017-2019 IQRF Tech s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types = 1);
+
+namespace App\ConsoleModule\Commands;
+
+use App\Models\Database\Entities\IqrfOsPatch;
+use App\Models\Database\EntityManager;
+use Nette\Utils\Finder;
+use SplFileInfo;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * IQRF OS patch import command
+ */
+class IqrfOsPatchImportCommand extends Command {
+
+	/**
+	 * Path to directory with IQRF OS patches
+	 */
+	private const DIR_PATH = __DIR__ . '/../../../iqrf/os/';
+
+	/**
+	 * @var string Command name
+	 */
+	protected static $defaultName = 'iqrf-os:import-patches';
+
+	/**
+	 * @var EntityManager Entity manager
+	 */
+	private $entityManager;
+
+	/**
+	 * Constructor
+	 * @param EntityManager $entityManager Entity manager
+	 * @param string|null $name Command name
+	 */
+	public function __construct(EntityManager $entityManager, ?string $name = null) {
+		parent::__construct($name);
+		$this->entityManager = $entityManager;
+	}
+
+	/**
+	 * Configures the user list command
+	 */
+	protected function configure(): void {
+		$this->setName(self::$defaultName);
+		$this->setDescription('Imports IQRF OS patches');
+	}
+
+
+	/**
+	 * Executes the database create command
+	 * @param InputInterface $input Command input
+	 * @param OutputInterface $output Command output
+	 * @return int Exit code
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$repository = $this->entityManager->getIqrfOsPatchRepository();
+		foreach ($repository->findAll() as $patch) {
+			assert($patch instanceof IqrfOsPatch);
+			if (file_exists(self::DIR_PATH . $patch->getFileName())) {
+				continue;
+			}
+			$output->writeln('Deleting IQRF OS patch' . $patch->getFileName());
+			$this->entityManager->remove($patch);
+			$this->entityManager->flush();
+		}
+		foreach (Finder::findFiles('*.iqrf')->in(self::DIR_PATH) as $file) {
+			assert($file instanceof SplFileInfo);
+			$fileName = $file->getFilename();
+			if ($repository->findOneBy(['fileName' => $fileName]) !== null) {
+				$output->writeln('IQRF OS patch ' . $file->getFilename() . ' has been already imported.');
+				continue;
+			}
+			$output->writeln('Importing IQRF OS patch ' . $file->getFilename());
+			$array = explode('-', $file->getBasename('.iqrf'));
+			[$fromVersion, $fromBuild] = sscanf($array[2], '%3d(%4x)');
+			[$toVersion, $toBuild] = sscanf($array[3], '%3d(%4x)');
+			if (count($array) === 5) {
+				$partArr = explode('of', $array[4]);
+				$part = (int) $partArr[0];
+				$parts = (int) $partArr[1];
+			} else {
+				$parts = $part = 1;
+			}
+			$patch = new IqrfOsPatch($array[1], $fromVersion, $fromBuild, $toVersion, $toBuild, $part, $parts, $fileName);
+			$this->entityManager->persist($patch);
+			$this->entityManager->flush();
+		}
+		return 0;
+	}
+
+}
