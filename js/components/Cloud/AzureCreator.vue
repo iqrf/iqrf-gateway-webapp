ae5a28762dd7a09c3a29f752d43e156801d9392e
Karel Hanák <khgreav@gmail.com>
2020-09-19 07:27:06 +0200

    AzureCreator: chain requests with timeouts

--- js/components/Cloud/AzureCreator.vue
+++ js/components/Cloud/AzureCreator.vue
@@ -38,13 +38,13 @@
 </template>
 
 <script>
+import axios from 'axios';
 import {CButton, CCard, CCardBody, CCardHeader, CForm, CInput} from '@coreui/vue';
 import {extend, ValidationObserver, ValidationProvider} from 'vee-validate';
 import {required} from 'vee-validate/dist/rules';
+import {authorizationHeader} from '../../helpers/authorizationHeader';
 import FormErrorHandler from '../../helpers/FormErrorHandler';
-import {timeout} from '../../helpers/timeout';
 import CloudService from '../../services/CloudService';
-import ServiceService from '../../services/ServiceService';
 
 export default {
 	name: 'AzureCreator',
@@ -62,8 +62,6 @@ export default {
 		return {
 			connectionString: null,
 			serviceName: 'azure',
-			timeout: null,
-			restart: false,
 		};
 	},
 	created() {
@@ -72,35 +70,36 @@ export default {
 	methods: {
 		save() {
 			this.$store.commit('spinner/SHOW');
-			this.timeout = timeout('cloud.messages.timeout', 10000);
-			CloudService.create(this.serviceName, {'connectionString': this.connectionString})
+			CloudService.createWithTimeout(this.serviceName, {'connectionString': this.connectionString}, 10000)
 				.then(() => {
-					clearTimeout(this.timeout);
 					this.$store.commit('spinner/HIDE');
 					this.$toast.success(this.$t('cloud.messages.success'));
-					if (this.restart) {
-						this.restart = false;
-						this.$store.commit('spinner/SHOW');
-						this.timeout = timeout('service.errors.processTimeout', 10000);
-						ServiceService.restart('iqrf-gateway-daemon')
-							.then(() => {
-								clearTimeout(this.timeout);
-								this.$store.commit('spinner/HIDE');
-								this.$toast.success(this.$t('service.iqrf-gateway-daemon.messages.restart'));
-							})
-							.catch((error) => {
-								clearTimeout(this.timeout);
-								FormErrorHandler.serviceError(error);
-							});
-					}
+				
 				})
 				.catch((error) => {
-					clearTimeout(this.timeout);
 					FormErrorHandler.cloudError(error);
 				});
 		},
 		saveAndRestart() {
-			this.restart = true;
+			axios.interceptors.response.use(
+				(response) => {
+					if (response.config.url === 'clouds/azure') {
+						this.$store.commit('spinner/SHOW');
+						axios.post('services/iqrf-gateway-daemon/restart', null, {headers: authorizationHeader(), timeout: 20000})
+							.then(() => {
+								this.$store.commit('spinner/HIDE');
+								this.$toast.success(this.$t('service.iqrf-gateway-daemon.messages.restart'));
+							})
+							.catch((error) => {
+								FormErrorHandler.serviceError(error);
+							});
+					}
+					return response;
+				},
+				(error) => {
+					return Promise.reject(error);
+				}
+			);
 			this.save();
 		}
 	}
