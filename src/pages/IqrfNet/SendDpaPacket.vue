d4c9d363571f73e7a623ce6bf0de8d3929693991
Karel Hanák <karel.hanak@microrisc.com>
2020-10-29 17:48:40 +0100

    IqrfNet: Add packet sections to SendDpaPacket component
    Signed-off-by: Karel Hanák <karel.hanak@microrisc.com>

--- src/pages/IqrfNet/SendDpaPacket.vue
+++ src/pages/IqrfNet/SendDpaPacket.vue
@@ -4,19 +4,100 @@
 		<CCard body-wrapper>
 			<ValidationObserver v-slot='{ invalid }'>
 				<CForm @submit.prevent='handleSubmit'>
-					<ValidationProvider
-						v-slot='{ valid, touched, errors }'
-						rules='dpaPacket|required'
-						:custom-messages='{required: "iqrfnet.sendPacket.form.messages.missing.packet"}'
-					>
-						<CInput
-							v-model='packet'
-							:label='$t("iqrfnet.sendPacket.form.packet")'
-							:is-valid='touched ? valid : null'
-							:invalid-feedback='$t(errors[0])'
-							@input='setTimeout'
-						/>
-					</ValidationProvider>
+					<CRow>
+						<CCol md='6'>
+							<ValidationProvider
+								v-slot='{valid, touched, errors}'
+								rules='nadr|minLen:4|maxLen:4|required'
+								:custom-messages='{
+									nadr: "iqrfnet.sendPacket.form.messages.invalid.nadr",
+									minLen: "iqrfnet.sendPacket.form.messages.invalid.nadr",
+									maxLen: "iqrfnet.sendPacket.form.messages.invalid.nadr",
+									required: "iqrfnet.sendPacket.form.messages.invalid.nadr"
+								}'
+							>
+								<CInput
+									v-model='packetNadr'
+									maxlength='4'
+									:label='$t("iqrfnet.sendPacket.form.nadr")'
+									:is-valid='touched ? valid : null'
+									:invalid-feedback='$t(errors[0])'
+									:disabled='addressOverwrite'
+									style='float: left; width: 25%'
+								/>
+							</ValidationProvider>
+							<ValidationProvider
+								v-slot='{valid, touched, errors}'
+								rules='pnum|minLen:2|maxLen:2|required'
+								:custom-messages='{
+									pnum: "iqrfnet.sendPacket.form.messages.invalid.pnum",
+									minLen: "iqrfnet.sendPacket.form.messages.invalid.pnum",
+									maxLen: "iqrfnet.sendPacket.form.messages.invalid.pnum",
+									required: "iqrfnet.sendPacket.form.messages.invalid.pnum",
+								}'
+							>
+								<CInput
+									v-model='packetPnum'
+									:label='$t("iqrfnet.sendPacket.form.pnum")'
+									:is-valid='touched ? valid : null'
+									:invalid-feedback='$t(errors[0])'
+									style='float: left; width: 25%'
+								/>
+							</ValidationProvider>
+							<ValidationProvider
+								v-slot='{valid, touched, errors}'
+								rules='pcmd|minLen:2|maxLen:2|required'
+								:custom-messages='{
+									pcmd: "iqrfnet.sendPacket.form.messages.invalid.pcmd",
+									minLen: "iqrfnet.sendPacket.form.messages.invalid.pcmd",
+									maxLen: "iqrfnet.sendPacket.form.messages.invalid.pcmd",
+									required: "iqrfnet.sendPacket.form.messages.invalid.pcmd",
+								}'
+							>
+								<CInput
+									v-model='packetPcmd'
+									:label='$t("iqrfnet.sendPacket.form.pcmd")'
+									:is-valid='touched ? valid : null'
+									:invalid-feedback='$t(errors[0])'
+									style='float: left; width: 25%'
+								/>
+							</ValidationProvider>
+							<ValidationProvider
+								v-slot='{valid, touched, errors}'
+								rules='hwpid|minLen:4|maxLen:4|required'
+								:custom-messages='{
+									hwpid: "iqrfnet.sendPacket.form.messages.invalid.hwpid",
+									minLen: "iqrfnet.sendPacket.form.messages.invalid.hwpid",
+									maxLen: "iqrfnet.sendPacket.form.messages.invalid.hwpid",
+									required: "iqrfnet.sendPacket.form.messages.invalid.hwpid",
+								}'
+							>
+								<CInput
+									v-model='packetHwpid'
+									:label='$t("iqrfnet.sendPacket.form.hwpid")'
+									:is-valid='touched ? valid : null'
+									:invalid-feedback='$t(errors[0])'
+									style='float: left; width: 25%'
+								/>
+							</ValidationProvider>
+						</CCol>
+						<CCol md='6'>
+							<ValidationProvider
+								v-slot='{valid, touched, errors}'
+								rules='pdata'
+								:custom-messages='{
+									pdata: "iqrfnet.sendPacket.form.messages.invalid.hwpid"
+								}'
+							>
+								<CInput
+									v-model='packetPdata'
+									:label='$t("iqrfnet.sendPacket.form.pdata")'
+									:is-valid='touched ? valid : null'
+									:invalid-feedback='$t(errors[0])'
+								/>
+							</ValidationProvider>
+						</CCol>
+					</CRow>
 					<CRow>
 						<CCol md='6'>
 							<CInputCheckbox
@@ -95,7 +176,7 @@ import {Component, Vue} from 'vue-property-decorator';
 import {MutationPayload} from 'vuex';
 import {CButton, CCard, CCardBody, CCardHeader, CCol, CForm, CInput, CInputCheckbox, CRow} from '@coreui/vue/src';
 import DpaMacros from '../../components/IqrfNet/DpaMacros.vue';
-import {between, integer, min_value, required} from 'vee-validate/dist/rules';
+import {between, integer, min_value, required, min, max} from 'vee-validate/dist/rules';
 import {extend, ValidationObserver, ValidationProvider} from 'vee-validate';
 import sendPacket from '../../iqrfNet/sendPacket';
 import JsonMessage from '../../components/IqrfNet/JsonMessage.vue';
@@ -143,9 +224,29 @@ export default class SendDpaPacket extends Vue {
 	private msgId: string|null = null
 
 	/**
-	 * @var {string} packet Raw packet string
+	 * @var {string} packetNadr Packet NADR bytes
 	 */
-	private packet = ''
+	private packetNadr = '0000'
+
+	/**
+	 * @var {string} packetPnum Packet PNUM byte
+	 */
+	private packetPnum = '00'
+
+	/**
+	 * @var {string} packetPcmd Packet PCMD byte
+ 	 */
+	private packetPcmd = '00'
+
+	/**
+	 * @var {string} packetHwpid Packet HWPID bytes
+	 */
+	private packetHwpid = 'ffff'
+
+	/**
+	 * @var {string} packetPdata Packet PDATA bytes
+	 */
+	private packetPdata = ''
 
 	/**
 	 * @var {string|null} request Daemon api request message, used in message card
@@ -173,15 +274,45 @@ export default class SendDpaPacket extends Vue {
 	private unsubscribe: CallableFunction = () => {return;}
 
 	/**
+	 * Computes packet string from packet parts
+	 * @returns {string} Packet string
+	 */
+	get packet(): string {
+		let packet = this.packetNadr.substr(0, 2) + '.' + this.packetNadr.substr(2, 2) + '.';
+		packet += this.packetPnum + '.' + this.packetPcmd + '.';
+		packet += this.packetHwpid.substr(0, 2) + '.' + this.packetHwpid.substr(2, 2) + '.' + this.packetPdata;
+		return packet;
+	}
+
+	/**
 	 * Vue lifecycle hook created
 	 */
 	created(): void {
 		extend('between', between);
 		extend('integer', integer);
 		extend('min', min_value);
+		extend('minLen', min);
+		extend('maxLen', max);
 		extend('required', required);
-		extend('dpaPacket', (packet) => {
-			return sendPacket.validatePacket(packet) ? true : 'iqrfnet.sendPacket.form.messages.invalid.packet';
+		extend('nadr', (nadr: string) => {
+			const re = new RegExp('^[0-9a-f]{2}00$', 'i');
+			return re.test(nadr);
+		});
+		extend('pnum', (pnum: string) => {
+			const re = new RegExp('^[0-9a-f]{2}$', 'i');
+			return re.test(pnum);
+		});
+		extend('pcmd', (pcmd: string) => {
+			const re = new RegExp('^[0-7][0-9a-f]$', 'i');
+			return re.test(pcmd);
+		});
+		extend('hwpid', (hwpid: string) => {
+			const re = new RegExp('^[0-9a-f]{4}$', 'i');
+			return re.test(hwpid);
+		});
+		extend('pdata', (pdata: string) => {
+			const re = new RegExp('^([0-9a-f]{2}\\.){0,56}[0-9a-f]{2}(\\.|)$', 'i');
+			return re.test(pdata);
 		});
 		this.unsubscribe = this.$store.subscribe((mutation: MutationPayload) => {
 			if (mutation.type === 'SOCKET_ONSEND' &&
@@ -273,11 +404,26 @@ export default class SendDpaPacket extends Vue {
 	}
 
 	/**
+	 * Removes dots from string packet representation
+	 * @param {string} bytes Packet bytes
+	 * @returns {string} Packet byte string without dot separators
+	 */
+	private joinBytes(bytes: string): string {
+		return bytes.replace(/\./g, '');
+	}
+
+	/**
 	 * Sets new DPA packet
 	 * @param {string} newPacket New DPA packet
 	 */
 	private setPacket(newPacket: string): void {
-		this.packet = newPacket;
+		this.packetNadr = this.joinBytes(newPacket.substr(0, 6));
+		this.packetPnum = this.joinBytes(newPacket.substr(6, 3));
+		this.packetPcmd = this.joinBytes(newPacket.substr(9, 3));
+		this.packetHwpid = this.joinBytes(newPacket.substr(12, 5));
+		if (newPacket.length > 17) {
+			this.packetPdata = newPacket.substr(18, newPacket.length - 1);
+		}
 		this.setTimeout();
 	}
 
