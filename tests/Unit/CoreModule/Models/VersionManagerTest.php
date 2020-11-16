4eeed730ec83c5d68d93dd5690304c92cbf7af1a
Karel Hanák <karel.hanak@microrisc.com>
2020-11-16 09:40:09 +0100

    Release version v2.2.0
    Signed-off-by: Karel Hanák <karel.hanak@microrisc.com>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -31,7 +31,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.2.0-rc3';
+	private const CURRENT_VERSION = '2.2.0';
 
 	/**
 	 * Current stable version of the webapp
