7a9bfce0fd83a4479b0e3a13dd088a3124559644
Karel Hanák <karel.hanak@microrisc.com>
2020-11-16 09:45:31 +0100

    Prepare for version v2.3.0-alpha
    Signed-off-by: Karel Hanák <karel.hanak@microrisc.com>

--- tests/Unit/GatewayModule/Models/VersionManagerTest.php
+++ tests/Unit/GatewayModule/Models/VersionManagerTest.php
@@ -73,7 +73,7 @@ final class VersionManagerTest extends WebSocketTestCase {
 	/**
 	 * IQRF Gateway Webapp's version
 	 */
-	private const WEBAPP_VERSION = 'v2.2.0';
+	private const WEBAPP_VERSION = 'v2.3.0-alpha';
 
 	/**
 	 * Sets up the test environment
