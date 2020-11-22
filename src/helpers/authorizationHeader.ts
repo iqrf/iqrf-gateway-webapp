56c26f21e1e8d66a553ad20ad494b1c854717b69
Roman Ondráček <ondracek.roman@centrum.cz>
2020-11-23 00:04:06 +0100

    Vue.js: fix TypeScript errors
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- src/helpers/authorizationHeader.ts
+++ src/helpers/authorizationHeader.ts
@@ -1,10 +1,9 @@
-import { Dictionary } from 'vue-router/types/router';
 import store from '../store';
 
 /**
  * Creates authorization header object for REST API requests
  */
-export function authorizationHeader(): Dictionary<string> {
+export function authorizationHeader(): {'Authorization'?: string} {
 	const token = store.getters['user/getToken'];
 	if (token === null) {
 		return {};
--- src/pages/Core/SignIn.vue
+++ src/pages/Core/SignIn.vue
@@ -136,7 +136,7 @@ export default class SignIn extends Vue {
 			this.$store.dispatch('features/fetch'),
 		])
 			.then(() => {
-				this.$router.push(this.$route.query.redirect ?? '/');
+				this.$router.push((this.$route.query.redirect as string|undefined) ?? '/');
 				this.$toast.success(
 					this.$t('core.sign.in.messages.success').toString()
 				);
