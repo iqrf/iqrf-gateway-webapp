99da1989d4570a12053db87233b5562526ab50bf
Karel Hanák <karel.hanak@iqrf.org>
2021-01-10 14:12:04 +0100

    Iqrfnet: update spinner messages during hex and dpa upload
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/components/IqrfNet/HexUpload.vue
+++ src/components/IqrfNet/HexUpload.vue
@@ -30,13 +30,16 @@
 <script lang='ts'>
 import {Component, Vue} from 'vue-property-decorator';
 import {CButton, CCard, CCardBody, CCardHeader, CForm, CInputFile, CSelect} from '@coreui/vue/src';
-import {FileFormat} from '../../iqrfNet/fileFormat';
+
 import {AxiosResponse, AxiosError} from 'axios';
 import {FileUpload} from '../../interfaces/trUpload';
-import NativeUploadService from '../../services/NativeUploadService';
-import IqrfService from '../../services/IqrfService';
-import FormErrorHandler from '../../helpers/FormErrorHandler';
 
+import {FileFormat} from '../../iqrfNet/fileFormat';
+
+import FormErrorHandler from '../../helpers/FormErrorHandler';
+import IqrfService from '../../services/IqrfService';
+import NativeUploadService from '../../services/NativeUploadService';
+import ServiceService from '../../services/ServiceService';
 
 @Component({
 	components: {
@@ -70,6 +73,11 @@ export default class HexUpload extends Vue {
 	private fileUntouched = true
 
 	/**
+	 * @var {boolean} uploadMessage Spinner text
+	 */
+	private uploadMessage = '';
+
+	/**
 	 * Extracts uploaded files from form file input
 	 * @returns {Filelist} list of uploaded files
 	 */
@@ -82,6 +90,7 @@ export default class HexUpload extends Vue {
 	 * Attempts to upload provided file to the gateway file storage
 	 */
 	private submitUpload(): void {
+		// build form data
 		const formData = new FormData();
 		formData.append('format', FileFormat.HEX);
 		const files = this.getFiles();
@@ -91,29 +100,87 @@ export default class HexUpload extends Vue {
 			);
 			return;
 		}
-		formData.append('file', this.getFiles()[0]);
+		formData.append('file', files[0]);
+
+		// status message and spinner update
+		this.uploadMessage = this.$t(
+			'iqrfnet.trUpload.hexUpload.messages.gatewayUpload',
+			{file: files[0].name}
+		).toString();
 		this.$store.commit('spinner/SHOW');
+		this.$store.commit('spinner/UPDATE_TEXT', this.uploadMessage);
+
+		// REST API upload
 		NativeUploadService.uploadREST(formData)
-			.then((response: AxiosResponse) => this.uploadFile(response.data))
+			.then((response: AxiosResponse) => {
+				this.uploadMessage += '\n' + this.$t(
+					'iqrfnet.trUpload.hexUpload.messages.gatewayUploadSuccess',
+					{file: files[0].name}
+				).toString();
+				this.$store.commit('spinner/UPDATE_TEXT', this.uploadMessage);
+				this.stopDaemon(response.data);
+			})
 			.catch(() => {
 				this.$store.commit('spinner/HIDE');
 				this.$toast.error(
-					this.$t('iqrfnet.trUpload.messages.failure').toString()
+					this.$t('iqrfnet.trUpload.messages.gatewayUploadFailure').toString()
 				);
 			});
 	}
 
 	/**
-	 * Sends IQRF Upload Utility REST API request to upload Custom DPA handler
+	 * Sends IQRF Gateway Uploader REST API request to upload Custom DPA handler
 	 * @param {FileUpload} response REST API response containing uploaded file metadata
 	 */
 	private uploadFile(response: FileUpload): void {
+		// update spinner status
+		this.uploadMessage += '\n' + this.$t(
+			'iqrfnet.trUpload.osUpload.messages.fileUploading',
+			{file: response.fileName}
+		).toString();
+		this.$store.commit('spinner/UPDATE_TEXT', this.uploadMessage);
+
+		// run uploader
 		IqrfService.uploader({name: response.fileName, type: 'HEX'})
 			.then(() => {
-				this.$store.commit('spinner/HIDE');
-				this.$toast.success(this.$t('iqrfnet.trUpload.hexUpload.messages.uploadSuccess').toString());
+				this.uploadMessage += '\n' + this.$t(
+					'iqrfnet.trUpload.osUpload.messages.fileUploaded',
+					{file: response.fileName}
+				).toString();
+				this.$store.commit('spinner/UPDATE_TEXT', this.uploadMessage);
+				this.startDaemon();
 			})
 			.catch((error: AxiosError) => FormErrorHandler.uploadUtilError(error));
+	}
+
+	/**
+	 * Stops the IQRF Daemon service before upgrading OS
+	 * @param {FileUpload} response Files to be uploaded
+	 */
+	private stopDaemon(response: FileUpload): void {
+		ServiceService.stop('iqrf-gateway-daemon')
+			.then(() => {
+				this.uploadMessage += '\n' + this.$t(
+					'service.iqrf-gateway-daemon.messages.stop'
+				).toString();
+				this.$store.commit('spinner/UPDATE_TEXT', this.uploadMessage);
+				this.uploadFile(response);
+			})
+			.catch((error: AxiosError) => FormErrorHandler.serviceError(error));
+	}
+
+	/**
+	 * Starts the IQRF Daemon service upon successful OS upgrade
+	 */
+	private startDaemon(): void {
+		ServiceService.start('iqrf-gateway-daemon')
+			.then(() => {
+				this.$store.commit('spinner/HIDE');
+				this.$toast.success(
+					this.$t('iqrfnet.trUpload.hexUpload.uploadSuccess').toString()
+				);
+			})
+			.catch((error: AxiosError) => FormErrorHandler.serviceError(error));
 	}
 
 	/**
