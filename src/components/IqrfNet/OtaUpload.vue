b5ade0417ad2879f0f72f52bf55d1fa88345fb96
Karel Hanák <karel.hanak@iqrf.org>
2021-01-19 10:29:25 +0100

    update npm packages

---
+++ src/components/IqrfNet/OtaUpload.vue
@@ -0,0 +1,24 @@
+<template>
+	<div />
+</template>
+
+<script lang='ts'>
+import {Component, Vue} from 'vue-property-decorator';
+import {CButton, CCard, CCardBody, CCardHeader, CForm, CInput, CSelect} from '@coreui/vue/src';
+
+@Component({
+	components: {
+		CButton,
+		CCard,
+		CCardBody,
+		CCardHeader,
+		CForm,
+		CInput,
+		CSelect,
+	}
+})
+
+export default class OtaUpload extends Vue {
+
+}
+</script>
