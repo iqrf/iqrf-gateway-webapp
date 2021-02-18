6867a1e3371159a39d0e91e7d0a5b62e3b829fe5
Karel Hanák <karel.hanak@iqrf.org>
2021-02-18 16:10:36 +0100

    Release version v2.2.6
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -31,7 +31,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.2.5';
+	private const CURRENT_VERSION = '2.2.6';
 
 	/**
 	 * Current stable version of the webapp
