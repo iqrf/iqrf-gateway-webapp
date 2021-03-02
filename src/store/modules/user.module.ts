7b3e2615c2ca77821232c1f84ee4a6145964c22f
Karel Hanák <karel.hanak@iqrf.org>
2021-03-02 17:26:25 +0100

    User module: fixed token expiration validation
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/store/modules/user.module.ts
+++ src/store/modules/user.module.ts
@@ -1,22 +1,36 @@
 import AuthenticationService, {User} from '../../services/AuthenticationService';
 import {ActionTree, GetterTree, MutationTree} from 'vuex';
 import {AxiosError} from 'axios';
+import jwt_decode, {JwtPayload} from 'jwt-decode';
 
 /**
  * User state
  */
 interface UserState {
-	user: User|null;
+	user: User|null,
+	expiration: number,
 }
 
 const state: UserState = {
 	user: null,
+	expiration: 0,
 };
 
 const actions: ActionTree<UserState, any> = {
 	signIn({commit}, credentials) {
 		return AuthenticationService.login(credentials)
 			.then((user: User) => {
+				const now = new Date();
+				const epoch = Math.round(now.getTime() / 1000);
+				const jwt: JwtPayload = jwt_decode(user.token);
+				if (jwt.exp === undefined) {
+					return Promise.reject(new Error('Expiration missing in JWT token.'));
+				}
+				if (jwt.iat === undefined) {
+					return Promise.reject(new Error('Token issue timestamp missing in JWT token.'));
+				}
+				const diff = epoch - jwt.iat;
+				commit('SET_EXPIRATION', jwt.exp + diff);
 				commit('SIGN_IN', user);
 			})
 			.catch((error: AxiosError) => {
@@ -31,7 +45,9 @@ const actions: ActionTree<UserState, any> = {
 
 const getters: GetterTree<UserState, any> = {
 	isLoggedIn(state: UserState) {
-		return state.user !== null;
+		const now = new Date();
+		const epoch = Math.round(now.getTime() / 1000);
+		return (state.user !== null && state.expiration > epoch);
 	},
 	getId(state: UserState) {
 		if (state.user === null) {
@@ -60,11 +76,15 @@ const getters: GetterTree<UserState, any> = {
 };
 
 const mutations: MutationTree<UserState> = {
+	SET_EXPIRATION(state: UserState, expiration: number) {
+		state.expiration = expiration;
+	},
 	SIGN_IN(state: UserState, data: User) {
 		state.user = data;
 	},
 	SIGN_OUT(state: UserState) {
 		state.user = null;
+		state.expiration = 0;
 	},
 };
 
