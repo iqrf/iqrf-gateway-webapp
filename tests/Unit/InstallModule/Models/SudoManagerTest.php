75489013e48bd59e209feb4bf73f6560c4fe02b2
Karel Hanák <karel.hanak@iqrf.org>
2021-03-07 18:27:12 +0100

    InstallModule models tests
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

---
+++ tests/Unit/InstallModule/Models/SudoManagerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * TEST: App\InstallModule\Models\SudoManager
+ * @covers App\InstallModule\Models\SudoManager
+ * @phpVersion >= 7.3
+ * @testCase
+ */
+declare(strict_types = 1);
+
+namespace Tests\Unit\InstallModule\Models;
+
+use App\InstallModule\Models\SudoManager;
+use Tester\Assert;
+use Tests\Stubs\CoreModule\Models\Command;
+use Tests\Toolkit\TestCases\CommandTestCase;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+/**
+ * Tests for Sudo manager
+ */
+final class SudoManagerTest extends CommandTestCase {
+
+	private const COMMAND = 'sudo -v';
+
+	/**
+	 * @var SudoManager Sudo manager
+	 */
+	private $manager;
+
+	/**
+	 * Sets up the test environment
+	 */
+	protected function setUp(): void {
+		parent::setUp();
+		$this->manager = new SudoManager($this->commandManager);
+	}
+
+	/**
+	 * Tests the function to check sudo and if webapp can use sudo
+	 */
+	public function testCheckSudo(): void {
+		$command = new Command(self::COMMAND, '', '', 0);
+		$this->commandManager->shouldReceive('commandExist')
+			->withArgs(['sudo'])
+			->andReturn(true);
+		$this->commandManager->shouldReceive('run')
+			->withArgs([self::COMMAND])
+			->andReturn($command);
+		$posix_user_info = posix_getpwnam('www-data');
+		posix_seteuid($posix_user_info['uid']);
+		posix_setegid($posix_user_info['gid']);
+		$expected = [
+			'user' => 'www-data',
+			'exists' => true,
+			'userSudo' => true,
+		];
+		Assert::same($expected, $this->manager->checkSudo());
+	}
+
+	/**
+	 * Tests the function to check sudo and if webapp can use sudo with root
+	 */
+	public function testCheckSudoRoot(): void {
+		Assert::same([], $this->manager->checkSudo());
+	}
+
+}
+
+$test = new SudoManagerTest();
+$test->run();
