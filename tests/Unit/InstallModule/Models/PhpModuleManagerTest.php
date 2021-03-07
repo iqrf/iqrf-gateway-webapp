08f4e488b86b007f5e63f44ba65eba88a4e81e60
Karel Hanák <karel.hanak@iqrf.org>
2021-03-07 18:27:12 +0100

    InstallModule models tests
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

---
+++ tests/Unit/InstallModule/Models/PhpModuleManagerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * TEST: App\InstallModule\Models\SudoManager
+ * @covers App\InstallModule\Models\SudoManager
+ * @phpVersion >= 7.3
+ * @testCase
+ */
+declare(strict_types = 1);
+
+namespace Tests\Unit\InstallModule\Models;
+
+use App\InstallModule\Models\PhpModuleManager;
+use Tester\Assert;
+use Tester\TestCase;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+/**
+ * Tests for PHP modules manager
+ */
+final class PhpModuleManagerTest extends TestCase {
+
+	/**
+	 * Sets up the test environment
+	 */
+	protected function setUp(): void {
+		parent::setUp();
+	}
+
+	/**
+	 * Tests the function to check installed and loaded PHP modules
+	 */
+	public function testCheckModules(): void {
+		Assert::noError(function(): void {
+			PhpModuleManager::checkModules();
+		});
+	}
+
+}
+
+$test = new PhpModuleManagerTest();
+$test->run();
