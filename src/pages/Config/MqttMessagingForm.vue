a6750611813e73562dc00234d875c0f9c1692608
Karel Hanák <karel.hanak@iqrf.org>
2021-03-13 14:53:04 +0100

    MqttMessagingForm: move tls switch
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/pages/Config/MqttMessagingForm.vue
+++ src/pages/Config/MqttMessagingForm.vue
@@ -225,7 +225,7 @@
 							</CCol>
 						</CRow>
 						<CRow>
-							<CCol md='6'>
+							<CCol>
 								<label style='font-size: 1.5rem'>
 									{{ $t('config.daemon.messagings.tlsTitle') }}
 								</label>
