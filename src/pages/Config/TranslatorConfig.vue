7d5ebd538ba6a21a22d1f8f28f21d5a2f47afc88
Karel Hanák <karel.hanak@iqrf.org>
2021-03-13 14:51:41 +0100

    Translator: updated mqtt client config form layout, added password visibility
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/pages/Config/TranslatorConfig.vue
+++ src/pages/Config/TranslatorConfig.vue
@@ -3,142 +3,13 @@
 		<h1>{{ $t('config.translator.title') }}</h1>
 		<CCard>
 			<CCardBody>
+				<CElementCover v-if='loadFailed && config === null' color='danger'>
+					{{ $t('config.translator.messages.loadFailed') }}
+				</CElementCover>
 				<ValidationObserver v-if='config !== null' v-slot='{invalid}'>
 					<CForm @submit.prevent='processSubmit'>
 						<CRow>
-							<CCol md='6'>
-								<h3>{{ $t("config.translator.form.mqtt.title") }}</h3>
-								<ValidationProvider 
-									v-slot='{errors, touched, valid}' 
-									rules='required'
-									:custom-messages='{
-										required: "config.translator.errors.brokerAddr"
-									}'
-								>
-									<CInput
-										v-model='config.mqtt.addr'
-										:label='$t("config.daemon.messagings.mqtt.form.BrokerAddr")'
-										:is-valid='touched ? valid : null'
-										:invalid-feedback='$t(errors[0])'
-									/>
-								</ValidationProvider>
-								<ValidationProvider 
-									v-slot='{errors, touched, valid}' 
-									rules='required|integer|between:1,49151'
-									:custom-messages='{
-										integer: "forms.errors.integer",
-										required: "config.translator.errors.port",
-										between: "config.translator.errors.port"
-									}'
-								>
-									<CInput
-										v-model.number='config.mqtt.port'
-										type='number'
-										min='1'
-										max='49151'
-										:label='$t("config.translator.form.mqtt.port")'
-										:is-valid='touched ? valid : null'
-										:invalid-feedback='$t(errors[0])'
-									/>
-								</ValidationProvider>
-								<ValidationProvider 
-									v-slot='{errors, touched, valid}' 
-									rules='required|clientId'
-									:custom-messages='{
-										required: "config.translator.errors.clientId",
-										clientId: "config.translator.errors.clientIdInvalid"
-									}'
-								>
-									<CInput
-										v-model='config.mqtt.cid'
-										:label='$t("forms.fields.clientId")'
-										:is-valid='touched ? valid : null'
-										:invalid-feedback='$t(errors[0])'								
-									/>
-								</ValidationProvider>
-								<ValidationProvider 
-									v-slot='{errors, touched, valid}' 
-									rules='required|requestTopic'
-									:custom-messages='{
-										required: "config.translator.errors.requestTopic",
-										requestTopic: "config.translator.errors.requestTopicInvalid"
-									}'
-								>
-									<CInput
-										v-model='config.mqtt.request_topic'
-										:label='$t("forms.fields.requestTopic")'
-										:is-valid='touched ? valid : null'
-										:invalid-feedback='$t(errors[0])'
-									/>
-								</ValidationProvider>
-								<ValidationProvider
-									v-slot='{errors, touched, valid}'
-									rules='required|responseTopic'
-									:custom-messages='{
-										required: "config.translator.errors.responseTopic",
-										responseTopic: "config.translator.errors.responseTopicInvalid"
-									}'
-								>
-									<CInput
-										v-model='config.mqtt.response_topic'
-										:label='$t("forms.fields.responseTopic")'
-										:is-valid='touched ? valid: null'
-										:invalid-feedback='$t(errors[0])'
-									/>
-								</ValidationProvider>
-								<ValidationProvider 
-									v-slot='{errors, touched, valid}'
-								>
-									<CInput
-										v-model='config.mqtt.user'
-										:label='$t("forms.fields.username")'
-										:is-valid='touched ? valid : null'
-										:invalid-feedback='$t(errors[0])'
-									/>
-								</ValidationProvider>
-								<ValidationProvider 
-									v-slot='{errors, touched, valid}'
-								>
-									<CInput
-										v-model='config.mqtt.pw'
-										:label='$t("forms.fields.password")'
-										:is-valid='touched ? valid : null'
-										:invalid-feedback='$t(errors[0])'
-										:type='visibility'
-									>
-										<template #append-content>
-											<span @click='changeVisibility'>
-												<CIcon
-													v-if='visibility ==="password"'
-													:content='icons.hidden'
-												/>
-												<CIcon v-else :content='icons.shown' />
-											</span>
-										</template>
-									</CInput>
-								</ValidationProvider>
-								<CInputCheckbox
-									:checked.sync='config.mqtt.tls.enabled'
-									:label='$t("config.translator.form.mqtt.tls.enable")'
-								/>
-								<CInput
-									v-model='config.mqtt.tls.trust_store'
-									:label='$t("config.translator.form.mqtt.tls.trustStore")'
-								/>
-								<CInput
-									v-model='config.mqtt.tls.key_store'
-									:label='$t("config.translator.form.mqtt.tls.keyStore")'
-								/>
-								<CInput
-									v-model='config.mqtt.tls.private_key'
-									:label='$t("config.translator.form.mqtt.tls.privateKey")'
-								/>
-								<CInputCheckbox
-									:checked.sync='config.mqtt.tls.require_broker_certificate'
-									:label='$t("config.translator.form.mqtt.tls.requireBrokerCert")'
-								/>
-							</CCol>
-							<CCol md='6'>
+							<CCol>
 								<h3>{{ $t("config.translator.form.rest.title") }}</h3>
 								<ValidationProvider
 									v-slot='{errors, touched, valid}'
@@ -190,14 +61,175 @@
 								</ValidationProvider>
 							</CCol>
 						</CRow>
+						<legend>{{ $t("config.translator.form.mqtt.title") }}</legend>
+						<ValidationProvider 
+							v-slot='{errors, touched, valid}' 
+							rules='required|clientId'
+							:custom-messages='{
+								required: "config.translator.errors.clientId",
+								clientId: "config.translator.errors.clientIdInvalid"
+							}'
+						>
+							<CInput
+								v-model='config.mqtt.cid'
+								:label='$t("forms.fields.clientId")'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'								
+							/>
+						</ValidationProvider>
+						<CRow>
+							<CCol md='6'>
+								<ValidationProvider 
+									v-slot='{errors, touched, valid}' 
+									rules='required'
+									:custom-messages='{
+										required: "config.translator.errors.brokerAddr"
+									}'
+								>
+									<CInput
+										v-model='config.mqtt.addr'
+										:label='$t("config.daemon.messagings.mqtt.form.BrokerAddr")'
+										:is-valid='touched ? valid : null'
+										:invalid-feedback='$t(errors[0])'
+									/>
+								</ValidationProvider>
+							</CCol>
+							<CCol md='6'>
+								<ValidationProvider 
+									v-slot='{errors, touched, valid}' 
+									rules='required|integer|between:1,49151'
+									:custom-messages='{
+										integer: "forms.errors.integer",
+										required: "config.translator.errors.port",
+										between: "config.translator.errors.port"
+									}'
+								>
+									<CInput
+										v-model.number='config.mqtt.port'
+										type='number'
+										min='1'
+										max='49151'
+										:label='$t("config.translator.form.mqtt.port")'
+										:is-valid='touched ? valid : null'
+										:invalid-feedback='$t(errors[0])'
+									/>
+								</ValidationProvider>
+							</CCol>
+							<CCol md='6'>
+								<ValidationProvider 
+									v-slot='{errors, touched, valid}' 
+									rules='required|requestTopic'
+									:custom-messages='{
+										required: "config.translator.errors.requestTopic",
+										requestTopic: "config.translator.errors.requestTopicInvalid"
+									}'
+								>
+									<CInput
+										v-model='config.mqtt.request_topic'
+										:label='$t("forms.fields.requestTopic")'
+										:is-valid='touched ? valid : null'
+										:invalid-feedback='$t(errors[0])'
+									/>
+								</ValidationProvider>
+							</CCol>
+							<CCol md='6'>
+								<ValidationProvider
+									v-slot='{errors, touched, valid}'
+									rules='required|responseTopic'
+									:custom-messages='{
+										required: "config.translator.errors.responseTopic",
+										responseTopic: "config.translator.errors.responseTopicInvalid"
+									}'
+								>
+									<CInput
+										v-model='config.mqtt.response_topic'
+										:label='$t("forms.fields.responseTopic")'
+										:is-valid='touched ? valid: null'
+										:invalid-feedback='$t(errors[0])'
+									/>
+								</ValidationProvider>
+							</CCol>
+							<CCol md='6'>
+								<ValidationProvider 
+									v-slot='{errors, touched, valid}'
+								>
+									<CInput
+										v-model='config.mqtt.user'
+										:label='$t("forms.fields.username")'
+										:is-valid='touched ? valid : null'
+										:invalid-feedback='$t(errors[0])'
+									/>
+								</ValidationProvider>
+							</CCol>
+							<CCol md='6'>
+								<ValidationProvider 
+									v-slot='{errors, touched, valid}'
+								>
+									<CInput
+										v-model='config.mqtt.pw'
+										:label='$t("forms.fields.password")'
+										:is-valid='touched ? valid : null'
+										:invalid-feedback='$t(errors[0])'
+										:type='visibility'
+									>
+										<template #append-content>
+											<span @click='visibility = (visibility === "password" ? "text" : "password")'>
+												<FontAwesomeIcon
+													:icon='(visibility === "password" ? ["far", "eye"] : ["far", "eye-slash"])'
+												/>
+											</span>
+										</template>
+									</CInput>
+								</ValidationProvider>
+							</CCol>
+						</CRow>
+						<CRow>
+							<CCol>
+								<h3 style='font-size: 1.5rem; float: left;'>
+									{{ $t('config.daemon.messagings.tlsTitle') }}
+								</h3>
+								<CSwitch
+									color='primary'
+									size='lg'
+									shape='pill'
+									label-on='ON'
+									label-off='OFF'
+									:checked.sync='config.mqtt.tls.enabled'
+									style='float: right;'
+								/>
+							</CCol>
+						</CRow>
+						<CRow v-if='config.mqtt.tls.enabled'>
+							<CCol md='6'>
+								<CInput
+									v-model='config.mqtt.tls.trust_store'
+									:label='$t("config.translator.form.mqtt.tls.trustStore")'
+								/>
+							</CCol>
+							<CCol md='6'>
+								<CInput
+									v-model='config.mqtt.tls.key_store'
+									:label='$t("config.translator.form.mqtt.tls.keyStore")'
+								/>
+							</CCol>
+							<CCol md='6'>
+								<CInput
+									v-model='config.mqtt.tls.private_key'
+									:label='$t("config.translator.form.mqtt.tls.privateKey")'
+								/>
+							</CCol>
+							<CCol md='6'>
+								<CInputCheckbox
+									:checked.sync='config.mqtt.tls.require_broker_certificate'
+									:label='$t("config.translator.form.mqtt.tls.requireBrokerCert")'
+								/>
+							</CCol>
+						</CRow>
 						<CButton color='primary' type='submit' :disabled='invalid'>
 							{{ $t('forms.save') }}
 						</CButton>
 					</CForm>
 				</ValidationObserver>
-				<CAlert v-else color='danger'>
-					{{ $t('config.translator.messages.loadFailed') }}
-				</CAlert>
 			</CCardBody>
 		</CCard>
 	</div>
@@ -205,28 +237,31 @@
 
 <script lang='ts'>
 import {Component, Vue} from 'vue-property-decorator';
-import {CAlert, CButton, CCard, CCardBody, CCardHeader, CForm, CIcon, CInput, CInputCheckbox} from '@coreui/vue/src';
-import {cilLockLocked, cilLockUnlocked} from '@coreui/icons';
-import {between, integer, required} from 'vee-validate/dist/rules';
+import {CButton, CCard, CCardBody, CCardHeader, CElementCover, CForm, CIcon, CInput, CInputCheckbox, CSwitch} from '@coreui/vue/src';
 import {extend, ValidationObserver, ValidationProvider} from 'vee-validate';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+
+import {between, integer, required} from 'vee-validate/dist/rules';
 import FormErrorHandler from '../../helpers/FormErrorHandler';
 import FeatureConfigService from '../../services/FeatureConfigService';
-import {NavigationGuardNext, Route} from 'vue-router';
-import {Dictionary} from 'vue-router/types/router';
+
 import {AxiosError, AxiosResponse} from 'axios';
 import {ITranslator} from '../../interfaces/translator';
+import {NavigationGuardNext, Route} from 'vue-router';
 
 @Component({
 	components: {
-		CAlert,
 		CButton,
 		CCard,
 		CCardBody,
 		CCardHeader,
+		CElementCover,
 		CForm,
 		CIcon,
 		CInput,
 		CInputCheckbox,
+		CSwitch,
+		FontAwesomeIcon,
 		ValidationObserver,
 		ValidationProvider
 	},
@@ -260,17 +295,14 @@ export default class TranslatorConfig extends Vue {
 	private visibility = 'password'
 
 	/**
+	 * @var {boolean} loadFailed Indicates whether configuration fetch failed
+	 */
+	private loadFailed = false
+
+	/**
 	 * @var {ITranslator|null} config IQRF Gateway Translator service configuration
 	 */
 	private config: ITranslator|null = null
-
-	/**
-	 * @constant {Dictionary<Array<string>>} icons Dictionary of CoreUI Icons
-	 */
-	private icons: Dictionary<Array<string>> = {
-		hidden: cilLockLocked,
-		shown: cilLockUnlocked
-	}
 
 	/**
 	 * Vue lifecycle hook created
@@ -307,14 +339,17 @@ export default class TranslatorConfig extends Vue {
 	/**
 	 * Retrieves configuration of IQRF Gateway Translator
 	 */
-	private getConfig(): void {
-		this.$store.commit('spinner/SHOW');
-		FeatureConfigService.getConfig(this.name)
+	private getConfig(): Promise<void> {
+		if (!this.$store.getters['spinner/isEnabled']) {
+			this.$store.commit('spinner/SHOW');
+		}
+		return FeatureConfigService.getConfig(this.name)
 			.then((response: AxiosResponse) => {
 				this.$store.commit('spinner/HIDE');
 				this.config = response.data;
 			})
 			.catch((error: AxiosError) => {
+				this.loadFailed = true;
 				FormErrorHandler.configError(error);
 			});
 	}
@@ -326,19 +361,14 @@ export default class TranslatorConfig extends Vue {
 		this.$store.commit('spinner/SHOW');
 		FeatureConfigService.saveConfig(this.name, this.config)
 			.then(() => {
-				this.$store.commit('spinner/HIDE');
-				this.$toast.success(this.$t('forms.messages.saveSuccess').toString());
+				this.getConfig().then(() => {
+					this.$store.commit('spinner/HIDE');
+					this.$toast.success(this.$t('forms.messages.saveSuccess').toString());
+				});
 			})
 			.catch((error: AxiosError) => {
 				FormErrorHandler.configError(error);
 			});
-	}
-
-	/**
-	 * Changes password input field visibility
-	 */
-	private changeVisibility(): void {
-		this.visibility = this.visibility === 'password' ? 'text' : 'password';
 	}
 }
 </script>
