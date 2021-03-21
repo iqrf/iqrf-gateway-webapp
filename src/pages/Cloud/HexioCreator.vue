59976e0ea065ec1cdf378b592cb34483ceba8097
Karel Hanák <karel.hanak@iqrf.org>
2021-03-21 11:33:17 +0100

    Cloud: updated hexio error handling
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/pages/Cloud/HexioCreator.vue
+++ src/pages/Cloud/HexioCreator.vue
@@ -177,7 +177,13 @@ export default class HexioCreator extends Vue {
 				this.$toast.success(this.$t('cloud.messages.success').toString());
 			})
 			.catch((error: AxiosError) => {
-				FormErrorHandler.cloudError(error);
+				this.$store.commit('spinner/HIDE');
+				this.$toast.error(
+					this.$t(
+						'cloud.hexio.messages.saveFailed',
+						{error: error.response ? error.response.data.message : error.message}
+					).toString()
+				);
 				return Promise.reject(error);
 			});
 	}
@@ -186,22 +192,26 @@ export default class HexioCreator extends Vue {
 	 * Stores new hexio cloud connection configuration in the gateway filesystem and restarts Daemon
 	 */
 	private saveAndRestart(): void {
-		this.save()
-			.then(() => {
-				this.$store.commit('spinner/SHOW');
-				ServiceService.restart('iqrf-gateway-daemon')
-					.then(() => {
-						this.$store.commit('spinner/HIDE');
-						this.$toast.success(
-							this.$t('service.iqrf-gateway-daemon.messages.restart')
-								.toString()
-						);
-					})
-					.catch((error: AxiosError) => {
-						FormErrorHandler.serviceError(error);
-					});
-			})
-			.catch(() => {return;});
+		this.save().then(() => {
+			this.$store.commit('spinner/SHOW');
+			ServiceService.restart('iqrf-gateway-daemon')
+				.then(() => {
+					this.$store.commit('spinner/HIDE');
+					this.$toast.success(
+						this.$t('service.iqrf-gateway-daemon.messages.restart')
+							.toString()
+					);
+				})
+				.catch((error: AxiosError) => {
+					this.$store.commit('spinner/HIDE');
+					this.$toast.error(
+						this.$t(
+							'service.messages.restartFailed',
+							{error: error.response ? error.response.data.message : error.message, service: 'IQRF Daemon'}
+						).toString()
+					);
+				});
+		});
 	}
 
 	/**
