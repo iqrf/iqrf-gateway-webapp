7c317c702064f76cd5a316bbbab8156f18334086
Roman Ondráček <ondracek.roman@centrum.cz>
2021-04-07 16:44:33 +0200

    Tests: fix IQRF Gateway Controller configuration manager tests
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Integration/ConfigModule/Models/ControllerConfigManagerTest.php
+++ tests/Integration/ConfigModule/Models/ControllerConfigManagerTest.php
@@ -27,11 +27,6 @@ require __DIR__ . '/../../../bootstrap.php';
 final class ControllerConfigManagerTest extends TestCase {
 
 	/**
-	 * Controller configuration file name
-	 */
-	private const FILE_NAME = 'config';
-
-	/**
 	 * Controller configuration directory path
 	 */
 	private const CONF_DIR = __DIR__ . '/../../../data/controller/';
@@ -40,6 +35,53 @@ final class ControllerConfigManagerTest extends TestCase {
 	 * Controller configuration temporary directory path
 	 */
 	private const TEMP_CONF_DIR = __DIR__ . '/../../../temp/controller/';
+
+	/**
+	 * IQRF Gateway Controller's expected configuration
+	 */
+	private const CONFIG = [
+		'daemonApi' => [
+			'autoNetwork' => [
+				'actionRetries' => 1,
+				'discoveryBeforeStart' => true,
+				'discoveryTxPower' => 6,
+				'skipDiscoveryEachWave' => false,
+				'stopConditions' => [
+					'abortOnTooManyNodesFound' => false,
+					'emptyWaves' => 2,
+					'waves' => 2,
+				],
+				'returnVerbose' => false,
+			],
+			'discovery' => [
+				'maxAddr' => 0,
+				'txPower' => 6,
+				'returnVerbose' => false,
+			],
+		],
+		'factoryReset' => [
+			'coordinator' => false,
+			'daemon' => true,
+			'network' => false,
+			'webapp' => true,
+		],
+		'logger' => [
+			'filePath' => '/var/log/iqrf-gateway-controller.log',
+			'severity' => 'info',
+		],
+		'resetButton' => [
+			'api' => '',
+			'button' => 2,
+		],
+		'statusLed' => [
+			'greenLed' => 0,
+			'redLed' => 1,
+		],
+		'wsServers' => [
+			'api' => 'ws://localhost:1338',
+			'monitor' => 'ws://localhost:1438',
+		],
+	];
 
 	/**
 	 * @var ControllerConfigManager Controller configuration manager
@@ -55,50 +97,7 @@ final class ControllerConfigManagerTest extends TestCase {
 	 * Tests the function to retrieve Controller configuration
 	 */
 	public function testGetConfig(): void {
-		$expected = [
-			'daemonApi' => [
-				'autoNetwork' => [
-					'actionRetries' => 1,
-					'discoveryBeforeStart' => true,
-					'discoveryTxPower' => 6,
-					'skipDiscoveryEachWave' => false,
-					'stopConditions' => [
-						'abortOnTooManyNodesFound' => false,
-						'emptyWaves' => 2,
-						'waves' => 2,
-					],
-					'returnVerbose' => false,
-				],
-				'discovery' => [
-					'maxAddr' => 0,
-					'txPower' => 6,
-					'returnVerbose' => false,
-				],
-			],
-			'factoryReset' => [
-				'coordinator' => false,
-				'daemon' => true,
-				'network' => false,
-				'webapp' => true,
-			],
-			'logger' => [
-				'filePath' => '/var/log/iqrf-gateway-controller.log',
-				'severity' => 'info',
-			],
-			'resetButton' => [
-				'api' => '',
-				'button' => 2,
-			],
-			'statusLed' => [
-				'greenLed' => 0,
-				'redLed' => 1,
-			],
-			'wsServers' => [
-				'api' => 'ws://localhost:1338',
-				'monitor' => 'ws://localhost:1438',
-			],
-		];
-		Assert::equal($expected, $this->manager->getConfig());
+		Assert::equal(self::CONFIG, $this->manager->getConfig());
 	}
 
 	/**
@@ -106,7 +105,8 @@ final class ControllerConfigManagerTest extends TestCase {
 	 */
 	public function testSaveConfig(): void {
 		Environment::lock('controller_config', __DIR__ . '/../../../temp/');
-		$expected = $this->manager->getConfig();
+		FileSystem::copy(self::CONF_DIR, self::TEMP_CONF_DIR);
+		$expected = self::CONFIG;
 		$expected['daemonApi']['discovery']['maxAddr'] = 5;
 		$expected['factoryReset']['coordinator'] = true;
 		$expected['logger']['severity'] = 'debug';
@@ -118,10 +118,9 @@ final class ControllerConfigManagerTest extends TestCase {
 	}
 
 	/**
-	 * Sets up the test enviornment
+	 * Sets up the test environment
 	 */
 	protected function setUp(): void {
-		FileSystem::copy(self::CONF_DIR, self::TEMP_CONF_DIR);
 		$commandStack = new CommandStack();
 		$commandManager = new CommandManager(false, $commandStack);
 		$fileManager = new JsonFileManager(self::CONF_DIR, $commandManager);
