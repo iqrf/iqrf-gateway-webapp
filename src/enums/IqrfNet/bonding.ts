792e3947e9ece72412845cc1e859f53a0cb82cde
Karel Hanák <karel.hanak@iqrf.org>
2021-05-21 15:31:52 +0200

    BondingManager: separated nfc bonding from bonding methods
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/enums/IqrfNet/bonding.ts
+++ src/enums/IqrfNet/bonding.ts
@@ -6,3 +6,18 @@ export enum BondingMethod {
 	SMARTCONNECT = 'smartconnect',
 	NFC = 'nfc'
 }
+
+/**
+ * Bonding targers
+ */
+export enum BondingTarget {
+	NODE = 'node',
+	SERVICETOOL = 'tool'
+}
+
+/**
+ * Bonding tools
+ */
+export enum Tool {
+	NFC = 'nfc'
+}
