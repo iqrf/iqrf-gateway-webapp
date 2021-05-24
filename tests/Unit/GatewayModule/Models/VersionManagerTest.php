2abc921c06960e6f29ab105ff2320a6e9b57710c
Karel Hanák <karel.hanak@iqrf.org>
2021-05-24 17:24:14 +0200

    Release version v2.3.0-rc4
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/GatewayModule/Models/VersionManagerTest.php
+++ tests/Unit/GatewayModule/Models/VersionManagerTest.php
@@ -98,7 +98,7 @@ final class VersionManagerTest extends WebSocketTestCase {
 	/**
 	 * IQRF Gateway Webapp's version
 	 */
-	private const WEBAPP_VERSION = 'v2.3.0-rc3';
+	private const WEBAPP_VERSION = 'v2.3.0-rc4';
 
 	/**
 	 * Sets up the test environment
