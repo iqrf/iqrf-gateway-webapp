6a7a4bbcc3790c0ee3823688504d5bcc36d2088b
Karel Hanák <karel.hanak@iqrf.org>
2021-06-08 08:13:44 +0200

    Tests: fixed test recognition
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/ServiceModule/Models/SystemDManagerTest.php
+++ tests/Unit/ServiceModule/Models/SystemDManagerTest.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
+ * TEST: App\ServiceModule\Models\SystemDManager
+ * @covers App\ServiceModule\Models\SystemDManager
+ * @phpVersion >= 7.3
+ * @testCase
+ */
+/**
  * Copyright 2017-2021 IQRF Tech s.r.o.
  * Copyright 2019-2021 MICRORISC s.r.o.
  *
@@ -15,12 +21,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-/**
- * TEST: App\ServiceModule\Models\SystemDManager
- * @covers App\ServiceModule\Models\SystemDManager
- * @phpVersion >= 7.2
- * @testCase
  */
 declare(strict_types = 1);
 
