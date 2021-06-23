d7417968789582bde1c35a75562124bde2bbef77
Roman Ondráček <ondracek.roman@centrum.cz>
2021-06-23 17:11:42 +0200

    IQRF net: fix condition in listing of IQRF OS upgrades
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- app/IqrfNetModule/Models/IqrfOsManager.php
+++ app/IqrfNetModule/Models/IqrfOsManager.php
@@ -94,7 +94,7 @@ class IqrfOsManager {
 			foreach ($this->osDpaManager->get($toBuild) as $dpa) {
 				$upgrade = $dpa->jsonSerialize();
 				$upgrade['osVersion'] = $toVersion;
-				if (hexdec($dpa->getDpa()) < 0x400) {
+				if (hexdec($dpa->getDpa()->getVersion()) < 0x400) {
 					$upgrade['dpa'] = $dpa->getDpa()->getVersion(true) . ', LP';
 					$versions[] = $upgrade;
 					$upgrade['dpa'] = $dpa->getDpa()->getVersion(true) . ', STD';
