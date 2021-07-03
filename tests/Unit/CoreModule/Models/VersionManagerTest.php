2a49c766c1365988be21c893d43536c3b17df6c0
Karel Hanák <karel.hanak@iqrf.org>
2021-07-03 14:09:28 +0200

    Release version v2.3.0-rc9
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.3.0-rc8';
+	private const CURRENT_VERSION = '2.3.0-rc9';
 
 	/**
 	 * Current stable version of the webapp
