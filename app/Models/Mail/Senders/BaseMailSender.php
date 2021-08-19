6d55586098a4384fc47e1249753ac49b720fb3c8
Roman Ondráček <ondracek.roman@centrum.cz>
2021-08-19 19:16:19 +0200

    Mail: refactor Latte templates, add base layout
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- app/Models/Mail/Senders/BaseMailSender.php
+++ app/Models/Mail/Senders/BaseMailSender.php
@@ -2,8 +2,10 @@
 
 declare(strict_types = 1);
 
-namespace App\Models\Mail;
+namespace App\Models\Mail\Senders;
 
+use App\Models\Mail\ConfigurationManager;
+use App\Models\Mail\MailerFactory;
 use Nette\Application\UI\Template;
 use Nette\Bridges\ApplicationLatte\TemplateFactory;
 use Nette\Localization\Translator;
@@ -13,6 +15,11 @@ use Nette\Mail\FallbackMailer;
  * Base e-mail sender
  */
 abstract class BaseMailSender {
+
+	/**
+	 * @var ConfigurationManager SMTP configuration manager
+	 */
+	protected $configuration;
 
 	/**
 	 * @var MailerFactory Mailer factory
@@ -31,11 +38,13 @@ abstract class BaseMailSender {
 
 	/**
 	 * Constructor
+	 * @param ConfigurationManager $configuration SMTP configuration manager
 	 * @param MailerFactory $mailerFactory Mailer factory
 	 * @param TemplateFactory $templateFactory Template factory
 	 * @param Translator $translator Translator
 	 */
-	public function __construct(MailerFactory $mailerFactory, TemplateFactory $templateFactory, Translator $translator) {
+	public function __construct(ConfigurationManager $configuration, MailerFactory $mailerFactory, TemplateFactory $templateFactory, Translator $translator) {
+		$this->configuration = $configuration;
 		$this->mailerFactory = $mailerFactory;
 		$this->templateFactory = $templateFactory;
 		$this->translator = $translator;
@@ -56,7 +65,6 @@ abstract class BaseMailSender {
 	protected function createTemplate(): Template {
 		$template = $this->templateFactory->createTemplate();
 		$latte = $template->getLatte();
-		$latte->addFilter('nl2br', 'nl2br');
 		$latte->addProvider('translator', $this->translator);
 		return $template;
 	}
