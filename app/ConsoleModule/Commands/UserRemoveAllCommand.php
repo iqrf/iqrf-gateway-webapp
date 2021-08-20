33eb29ca336f9e2d14d3476760792ec4b45ac048
Karel Hanák <karel.hanak@iqrf.org>
2021-08-20 12:54:05 +0200

    ConsoleModule: fixed remove all users command
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- app/ConsoleModule/Commands/UserRemoveAllCommand.php
+++ app/ConsoleModule/Commands/UserRemoveAllCommand.php
@@ -57,31 +57,63 @@ class UserRemoveAllCommand extends UserCommand {
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output): int {
 		$style = new SymfonyStyle($input, $output);
-		$style->title('Remove all users');
-		$role = $this->askRole($input, $output);
 		$users = [];
-		if ($role === 'all') {
+		$role = $this->askRole($input, $output);
+		if ($role === null) {
 			$users = $this->repository->findAll();
 		} else {
+			if (!in_array($role, User::ROLES, true)) {
+				$style->error('Role ' . $role . ' does not exist.');
+				return 1;
+			}
 			$users = $this->repository->findBy(['role' => $role]);
 		}
-		$helper = $this->getHelper('question');
-		$usernames = implode(', ', array_map(function (User $user): string {
-			return $user->getUserName();
-		}, $users));
-		if (strlen($usernames) !== 0) {
-			$usernames = ' [' . $usernames . ']';
-		}
-		$question = new ConfirmationQuestion('Do you really want to remove all selected users?' . $usernames, false);
-		if (!$helper->ask($input, $output, $question)) {
-			return 0;
+		if ($input->isInteractive() && $users !== []) {
+			$helper = $this->getHelper('question');
+			$question = new ConfirmationQuestion(sprintf('Do you really want to remove user(s) %s? (y/N)', $this->usersToString($users)), false);
+			if (!$helper->ask($input, $output, $question)) {
+				$style->warning('No users were removed.');
+				return 0;
+			}
 		}
 		foreach ($users as $user) {
 			assert($user instanceof User);
 			$this->entityManager->remove($user);
-			$this->entityManager->flush();
 		}
+		$this->entityManager->flush();
+		$this->printUsers($users, $style);
 		return 0;
+	}
+
+	/**
+	 * Converts array of users to a string of usernames
+	 * @param array<User> $users Array of users
+	 * @return string|null String of usernames
+	 */
+	private function usersToString(array $users): ?string {
+		if ($users === []) {
+			return null;
+		}
+		return implode(', ', array_map(function (User $user): string {
+			return $user->getUserName();
+		}, $users));
+	}
+
+	/**
+	 * Prints styled user remove success message
+	 * @param array<User> $users Array of users
+	 * @param SymfonyStyle $style Symfony style
+	 */
+	private function printUsers(array $users, SymfonyStyle $style): void {
+		if ($users === []) {
+			$style->success('There are no users with that role to remove.');
+			return;
+		}
+		if (count($users) === 1) {
+			$style->success('User ' . $this->usersToString($users) . ' was successfully removed.');
+			return;
+		}
+		$style->success('Users ' . $this->usersToString($users) . ' were successfully removed.');
 	}
 
 	/**
@@ -93,12 +125,16 @@ class UserRemoveAllCommand extends UserCommand {
 	private function askRole(InputInterface $input, OutputInterface $output): ?string {
 		$role = $input->getOption('role');
 		if (!$input->isInteractive()) {
-			return $role ?? 'all';
+			return $role;
 		}
-		$roles = array_merge(['all'], User::ROLES);
-		while ($role === null || !in_array($role, $roles, true)) {
+		while ($role === null || !in_array($role, User::ROLES, true)) {
 			$helper = $this->getHelper('question');
-			$question = new ChoiceQuestion('Please select a role to delete users by: ', $roles);
+			$question = new ConfirmationQuestion('Do you want to filter removed users by role? (y/N)', false);
+			if (!$helper->ask($input, $output, $question)) {
+				return null;
+			}
+			$helper = $this->getHelper('question');
+			$question = new ChoiceQuestion('Please select a role to delete users by: ', User::ROLES);
 			$role = $helper->ask($input, $output, $question);
 		}
 		return $role;
