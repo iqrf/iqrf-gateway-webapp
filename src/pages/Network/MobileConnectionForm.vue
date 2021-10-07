ad3582f7dc23cab4cdb0ce10ae74f41d1f2d05ac
Karel Hanák <karel.hanak@iqrf.org>
2021-10-07 18:21:04 +0200

    mobile connections form, added pin to gsm connection
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/pages/Network/MobileConnectionForm.vue
+++ src/pages/Network/MobileConnectionForm.vue
@@ -15,22 +15,164 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <template>
-	<div />
+	<div>
+		<h1>{{ pageTitle }}</h1>
+		<CCard>
+			<CCardBody>
+				<ValidationObserver v-slot='{invalid}'>
+					<CForm @submit.prevent='saveConnection'>
+						<ValidationProvider
+							v-slot='{errors, touched, valid}'
+							rules='required'
+							:custom-messages='{
+								required: "network.connection.errors.name"
+							}'
+						>
+							<CInput
+								v-model='connection.name'
+								:label='$t("network.connection.name")'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot='{errors, touched, valid}'
+							rules='required'
+							:custom-messages='{
+								required: "network.mobile.errors.interfaceMissing"
+							}'
+						>
+							<CSelect
+								:value.sync='connection.interface'
+								:label='$t("network.connection.interface")'
+								:placeholder='$t("network.mobile.form.interface")'
+								:options='interfaceOptions'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'
+							/>
+						</ValidationProvider>
+						<div class='form-group'>
+							<label for='autoconnect'>
+								<b>{{ $t("network.connection.autoconnect") }}</b>
+							</label><br>
+							<CSwitch
+								id='autoconnect'
+								size='lg'
+								shape='pill'
+								color='primary'
+								label-on='ON'
+								label-off='OFF'
+							/>
+						</div>
+						<legend>{{ $t('network.mobile.form.title') }}</legend>
+						<ValidationProvider
+							v-slot='{errors, touched, valid}'
+							rules='required|apn'
+							:custom-messages='{
+								required: "network.mobile.errors.apnMissing",
+								apn: "network.mobile.errors.apnInvalid"
+							}'
+						>
+							<CInput
+								v-model='connection.gsm.apn'
+								:label='$t("network.mobile.form.apn")'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot='{errors, touched, valid}'
+							rules='required'
+							:custom-messages='{
+								required: "network.mobile.errors.numberMissing"
+							}'
+						>
+							<CInput
+								v-model='connection.gsm.number'
+								:label='$t("network.mobile.form.number")'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot='{errors, touched, valid}'
+							:rules='{
+								required: connection.gsm.password.length > 0
+							}'
+							:custom-messages='{
+								required: "network.mobile.errors.credentialsMissing"
+							}'
+						>
+							<CInput
+								v-model='connection.gsm.username'
+								:rules='{
+									required: connection.gsm.password.length > 0
+								}'
+								:label='$t("forms.fields.username")'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot='{errors, touched, valid}'
+							:rules='{
+								required: connection.gsm.username.length > 0
+							}'
+							:custom-messages='{
+								required: "network.mobile.errors.credentialsMissing"
+							}'
+						>
+							<CInput
+								v-model='connection.gsm.password'
+								:label='$t("forms.fields.password")'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot='{errors, touched, valid}'
+							rules='pin'
+							:custom-messages='{
+								pin: "network.mobile.errors.pinInvalid"
+							}'
+						>
+							<CInput
+								v-model='connection.gsm.pin'
+								:label='$t("network.mobile.form.pin")'
+								:is-valid='touched ? valid : null'
+								:invalid-feedback='$t(errors[0])'
+							/>
+						</ValidationProvider>
+						<CButton
+							color='primary'
+							type='submit'
+							:disabled='invalid'
+						>
+							{{ $t('forms.save') }}
+						</CButton>
+					</CForm>
+				</ValidationObserver>
+			</CCardBody>
+		</CCard>
+	</div>
 </template>
 
 <script lang='ts'>
 import {Component, Prop, Vue} from 'vue-property-decorator';
-import {CButton, CCard, CCardBody, CForm} from '@coreui/vue/src';
+import {CButton, CCard, CCardBody, CForm, CInput, CSelect, CSwitch} from '@coreui/vue/src';
+import {extend, ValidationObserver, ValidationProvider} from 'vee-validate';
 
 import {extendedErrorToast} from '../../helpers/errorToast';
+import {required} from 'vee-validate/dist/rules';
+import {v4 as uuidv4} from 'uuid';
+import NetworkConnectionService from '../../services/NetworkConnectionService';
 import NetworkInterfaceService, {InterfaceType} from '../../services/NetworkInterfaceService';
 
 import {AxiosError, AxiosResponse} from 'axios';
+import {IConnection} from '../../interfaces/network';
 import {IOption} from '../../interfaces/coreui';
 import {MetaInfo} from 'vue-meta';
 import {NetworkInterface} from '../../interfaces/gatewayInfo';
-import NetworkConnectionService from '../../services/NetworkConnectionService';
-import { IConnection } from '../../interfaces/network';
 
 @Component({
 	components: {
@@ -38,6 +180,11 @@ import { IConnection } from '../../interfaces/network';
 		CCard,
 		CCardBody,
 		CForm,
+		CInput,
+		CSelect,
+		CSwitch,
+		ValidationObserver,
+		ValidationProvider,
 	},
 	metaInfo(): MetaInfo {
 		return {
@@ -57,11 +204,12 @@ export default class MobileConnectionForm extends Vue {
 	private connection: IConnection = {
 		autoConnect: {
 			enabled: true,
-			priority: 0,
+			priority: 700,
 			retries: -1,
 		},
 		name: '',
 		type: 'gsm',
+		interface: '',
 		ipv4: {
 			addresses: [],
 			dns: [],
@@ -73,7 +221,14 @@ export default class MobileConnectionForm extends Vue {
 			dns: [],
 			gateway: '',
 			method: 'ignore',
-		}
+		},
+		gsm: {
+			apn: '',
+			number: '',
+			username: '',
+			password: '',
+			pin: '',
+		},
 	}
 
 	/**
@@ -92,12 +247,25 @@ export default class MobileConnectionForm extends Vue {
 	 */
 	get pageTitle(): string {
 		return this.$t(
-			'network.mobile.' + (this.$route.path.includes('/mobile/add') ? 'add' : 'edit')
+			'network.mobile.' + (this.$route.path.includes('/add') ? 'add' : 'edit')
 		).toString();
 	}
 
 	/**
-	 *
+	 * Initializes validation rules
+	 */
+	created(): void {
+		extend('required', required);
+		extend('apn', (apn: string) => {
+			return new RegExp(/^[a-z0-9\\.\\-]+$/).test(apn);
+		});
+		extend('pin', (pin: string) => {
+			return new RegExp(/^[0-9]{4}$/).test(pin);
+		});
+	}
+
+	/**
+	 * Retrieve list of available interfaces and connection details if editing one
 	 */
 	mounted(): void {
 		this.getInterfaces();
@@ -116,6 +284,13 @@ export default class MobileConnectionForm extends Vue {
 				});
 				this.interfaceOptions = interfaces;
 				this.$store.commit('spinner/HIDE');
+				/*if (this.interfaceOptions.length === 0) {
+					this.$toast.error(
+						this.$t('network.mobile.messages.noInterfaces').toString()
+					);
+					this.$router.push('/network/mobile');
+					return;
+				}*/
 				if (this.uuid !== null) {
 					this.getConnection();
 				}
@@ -145,6 +320,51 @@ export default class MobileConnectionForm extends Vue {
 				);
 				this.$router.push('/network/mobile');
 			});
+	}
+
+	/**
+	 * Saves connection configuration
+	 */
+	private saveConnection(): void {
+		let connection: IConnection = JSON.parse(JSON.stringify(this.connection));
+		this.$store.commit('spinner/SHOW');
+		if (this.uuid === null) {
+			connection.uuid = uuidv4();
+			NetworkConnectionService.add(connection)
+				.then(() => this.handleSuccess(connection.name))
+				.catch(this.handleError);
+		} else {
+			NetworkConnectionService.edit(this.uuid, connection)
+				.then(() => this.handleSuccess(connection.name))
+				.catch(this.handleError);
+		}
+	}
+
+	/**
+	 * Handles connection save success
+	 */
+	private handleSuccess(name: string): void {
+		this.$store.commit('spinner/HIDE');
+		this.$toast.success(
+			this.$t(
+				'network.connection.messages.' +
+				(this.$route.path.includes('/add') ? 'add' : 'edit') + '.success',
+				{connection: name}).toString()
+		);
+		this.$router.push('/network/mobile');
+	}
+
+	/**
+	 * Handles connection save errors
+	 * @param {AxiosError} error Axios error
+	 */
+	private handleError(error: AxiosError): void {
+		extendedErrorToast(
+			error,
+			'network.connection.messages.' +
+			(this.$route.path.includes('/add') ? 'add' : 'edit') + '.failed'
+		);
+		this.$router.push('/network/mobile');
 	}
 }
 </script>
