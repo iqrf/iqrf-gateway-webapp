0bda541ead549cafa85612127c992b1c8359074c
Karel Hanák <karel.hanak@iqrf.org>
2021-11-21 14:47:43 +0100

    vue config: updated webpack svg rule
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- vue.config.js
+++ vue.config.js
@@ -20,14 +20,17 @@ module.exports = {
 	publicPath: baseUrl,
 	runtimeCompiler: true,
 	outputDir: 'www/dist',
-	chainWebpack: (config) => {
-		const svgRule = config.module.rule('svg');
-		svgRule.uses.clear();
-		svgRule
-			.use('babel-loader')
-			.loader('babel-loader')
-			.end()
-			.use('vue-svg-loader')
-			.loader('vue-svg-loader');
+	chainWebpack: config => {
+		config.module.rules.delete('svg');
 	},
+	configureWebpack: {
+		module: {
+			rules: [
+				{
+					test: /\.svg$/,
+					loader: 'vue-svg-loader',
+				},
+			],
+		}
+	}
 };
