935e322ac4754c2f2a1295f1f162119cec16c845
Roman Ondráček <ondracek.roman@centrum.cz>
2021-12-19 20:50:09 +0100

    Release version 2.4.0
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.0-rc3';
+	private const CURRENT_VERSION = '2.4.0';
 
 	/**
 	 * Current stable version of the webapp
