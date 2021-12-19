e99d673bd11ff529e7435d517c88c31ccad8103d
Roman Ondráček <ondracek.roman@centrum.cz>
2021-12-19 20:58:09 +0100

    Prepare for version 2.4.1-alpha
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.0';
+	private const CURRENT_VERSION = '2.4.1-alpha';
 
 	/**
 	 * Current stable version of the webapp
