b445ca706df6c98c0543206c6cb2b26abfe9a506
Karel Hanák <karel.hanak@iqrf.org>
2022-01-27 15:20:10 +0100

    GatewayBackup: configure select daemon components according to gwid
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- app/GatewayModule/Models/Backup/GatewayFileBackup.php
+++ app/GatewayModule/Models/Backup/GatewayFileBackup.php
@@ -21,6 +21,9 @@ declare(strict_types = 1);
 namespace App\GatewayModule\Models\Backup;
 
 use App\CoreModule\Models\ZipArchiveManager;
+use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
+use Nette\Utils\Strings;
 
 /**
  * Gateway file backup manager
@@ -38,6 +41,31 @@ class GatewayFileBackup implements IBackupManager {
 	 * Path to configuration directory
 	 */
 	private const CONF_PATH = '/etc/';
+
+	/**
+	 * Path to MqttMessaging component configuration
+	 */
+	private const MQTT_PATH = '/etc/iqrf-gateway-daemon/iqrf__MqttMessaging.json';
+
+	/**
+	 * Client ID pattern
+	 */
+	private const GWID_PATTERN = '/^[a-f0-9]{16}$/';
+
+	/**
+	 * MQTT request topic pattern
+	 */
+	private const REQUEST_TOPIC_PATTERN = '/^gateway\\/[a-z0-9]{16}\\/iqrf\\/requests$/';
+
+	/**
+	 * MQTT response topic pattern
+	 */
+	private const RESPONSE_TOPIC_PATTERN = '/^gateway\\/[a-z0-9]{16}\\/iqrf\\/responses$/';
+
+	/**
+	 * Path to JsonSplitter component configuration
+	 */
+	private const SPLITTER_PATH = '/etc/iqrf-gateway-daemon/iqrf__JsonSplitter.json';
 
 	/**
 	 * @var string Gateway ID
@@ -61,7 +89,7 @@ class GatewayFileBackup implements IBackupManager {
 	 * @param ZipArchiveManager $zipManager ZIP archive manager
 	 */
 	public function __construct(string $gwId, string $gwToken, ZipArchiveManager $zipManager) {
-		$this->gwId = $gwId;
+		$this->gwId = Strings::lower($gwId);
 		$this->gwToken = $gwToken;
 		$this->zipManager = $zipManager;
 	}
@@ -72,12 +100,12 @@ class GatewayFileBackup implements IBackupManager {
 	 * @param array<string, bool> $services Array of services
 	 */
 	public function backup(array $params, ?array &$services = null): void {
-		$this->gwId = $this->gwId;
-		$this->gwToken = $this->gwToken;
 		if (!$params['software']['iqrf']) {
 			return;
 		}
-		$this->zipManager->addFile(self::CONF_PATH . 'iqrf-gateway.json', 'gateway/iqrf-gateway.json');
+		if (file_exists(self::CONF_PATH . 'iqrf-gateway.json')) {
+			$this->zipManager->addFile(self::CONF_PATH . 'iqrf-gateway.json', 'gateway/iqrf-gateway.json');
+		}
 	}
 
 	/**
@@ -86,6 +114,24 @@ class GatewayFileBackup implements IBackupManager {
 	public function restore(): void {
 		if (!$this->zipManager->exist('gateway/')) {
 			return;
+		}
+		if (file_exists(self::MQTT_PATH)) {
+			$config = Json::decode(FileSystem::read(self::MQTT_PATH), Json::FORCE_ARRAY);
+			if (Strings::match($config['ClientId'], self::GWID_PATTERN) !== null) {
+				$config['ClientId'] = $this->gwId;
+			}
+			if (Strings::match($config['TopicRequest'], self::REQUEST_TOPIC_PATTERN) !== null) {
+				$config['TopicRequest'] = 'gateway/' . $this->gwId . '/iqrf/requests';
+			}
+			if (Strings::match($config['TopicResponse'], self::RESPONSE_TOPIC_PATTERN) !== null) {
+				$config['TopicResponse'] = 'gateway/' . $this->gwId . '/iqrf/responses';
+			}
+			FileSystem::write(self::MQTT_PATH, Json::encode($config));
+		}
+		if (file_exists(self::SPLITTER_PATH)) {
+			$config = Json::decode(FileSystem::read(self::SPLITTER_PATH), Json::FORCE_ARRAY);
+			$config['insId'] = $this->gwToken;
+			FileSystem::write(self::SPLITTER_PATH, Json::encode($config));
 		}
 	}
 
