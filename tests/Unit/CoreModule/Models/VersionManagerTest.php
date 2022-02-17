3416a54caf98901f737737234c1c1f22643022fa
Karel Hanák <karel.hanak@iqrf.org>
2022-02-17 13:32:48 +0100

    Release version 2.4.2
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.2-alpha';
+	private const CURRENT_VERSION = '2.4.2';
 
 	/**
 	 * Current stable version of the webapp
--- tests/Unit/GatewayModule/Models/VersionManagerTest.php
+++ tests/Unit/GatewayModule/Models/VersionManagerTest.php
@@ -114,7 +114,7 @@ final class VersionManagerTest extends WebSocketTestCase {
 	/**
 	 * IQRF Gateway Webapp's version
 	 */
-	private const WEBAPP_VERSION = 'v2.4.2-alpha';
+	private const WEBAPP_VERSION = 'v2.4.2';
 
 	/**
 	 * Sets up the test environment
