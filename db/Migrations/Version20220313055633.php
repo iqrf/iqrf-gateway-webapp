ca100e63b8ad6553b2b51e7098352c014d6f3ca4
Karel Hanák <karel.hanak@iqrf.org>
2022-03-13 08:06:43 +0100

    fixed coding style in migration file
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- db/Migrations/Version20220313055633.php
+++ db/Migrations/Version20220313055633.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Copyright 2017-2021 IQRF Tech s.r.o.
+ * Copyright 2019-2021 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 declare(strict_types=1);
 
 namespace Database\Migrations;
@@ -11,27 +27,27 @@ use Doctrine\Migrations\AbstractMigration;
  * User role update database migration
  */
 final class Version20220313055633 extends AbstractMigration {
-    /**
-     * Returns the migration description
-     * @return string Migration description
-     */
-    public function getDescription(): string {
-        return 'User role update database migration';
-    }
+	/**
+	 * Returns the migration description
+	 * @return string Migration description
+	 */
+	public function getDescription(): string {
+		return 'User role update database migration';
+	}
 
-    public function up(Schema $schema): void {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+	public function up(Schema $schema): void {
+		// this up() migration is auto-generated, please modify it to your needs
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('UPDATE "users" SET role="admin" WHERE role="normal" OR role="power"');
-        $this->addSql('UPDATE "users" SET role="basic" WHERE role="iqaros"');
-    }
+		$this->addSql('UPDATE "users" SET role="admin" WHERE role="normal" OR role="power"');
+		$this->addSql('UPDATE "users" SET role="basic" WHERE role="iqaros"');
+	}
 
-    public function down(Schema $schema): void {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+	public function down(Schema $schema): void {
+		// this down() migration is auto-generated, please modify it to your needs
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('UPDATE "users" SET role="normal" WHERE role="admin"');
-        $this->addSql('UPDATE "users" SET role="iqaros" WHERE role="basic"');
-    }
+		$this->addSql('UPDATE "users" SET role="normal" WHERE role="admin"');
+		$this->addSql('UPDATE "users" SET role="iqaros" WHERE role="basic"');
+	}
 }
