a3d7c7898f5f3dfa063b16aa84e7e57e2c4eef50
Karel Hanák <karel.hanak@iqrf.org>
2022-06-02 13:09:58 +0200

    Prepare for version 2.4.9-alpha
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.8';
+	private const CURRENT_VERSION = '2.4.9-alpha';
 
 	/**
 	 * Current stable version of the webapp
