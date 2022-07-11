4dbf4f0050ab376bc3cc40a619738031a3624aa8
Roman Ondráček <ondracek.roman@centrum.cz>
2022-07-12 01:15:44 +0200

    Maintainence: use Vuetify instead of CoreUI
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- src/pages/Maintenance/MenderControl.vue
+++ src/pages/Maintenance/MenderControl.vue
@@ -19,44 +19,7 @@ limitations under the License.
 		<h1>{{ $t('maintenance.mender.service.pageTitle') }}</h1>
 		<v-card>
 			<v-card-text>
-				<div class='box'>
-					<div>
-						<strong>{{ $t('service.status') }}</strong>
-					</div>
-					<div v-if='missing'>
-						{{ $t('service.states.missing') }}
-					</div>
-					<div v-if='unsupported'>
-						{{ $t('service.states.unsupported') }}
-					</div>
-					<div v-if='service !== null'>
-						{{ $t(`states.${service.enabled ? 'enabled' : 'disabled'}`) }},
-						{{ $t(`service.states.${service.active ? '' : 'in'}active`) }}
-					</div>
-					<div v-if='service !== null'>
-						<v-btn
-							v-if='!service.enabled'
-							color='success'
-							small
-							@click='enable()'
-						>
-							{{ $t('service.actions.enable') }}
-						</v-btn> <v-btn
-							v-if='service.enabled'
-							color='error'
-							small
-							@click='disable()'
-						>
-							{{ $t('service.actions.disable') }}
-						</v-btn> <v-btn
-							color='primary'
-							small
-							@click='restart()'
-						>
-							{{ $t('service.actions.restart') }}
-						</v-btn>
-					</div>
-				</div>
+				<ServiceControl service-name='mender-client' />
 				<MenderServiceForm />
 			</v-card-text>
 		</v-card>
@@ -65,17 +28,14 @@ limitations under the License.
 
 <script lang='ts'>
 import {Component, Vue} from 'vue-property-decorator';
-import MenderServiceForm from '@/components/Maintenance/MenderServiceForm.vue';
-
-import ServiceService, {ServiceStatus} from '@/services/ServiceService';
-import {menderErrorToast} from '@/helpers/errorToast';
-
-import {AxiosError} from 'axios';
 import {NavigationGuardNext, Route} from 'vue-router';
+import MenderServiceForm from '@/components/Maintenance/MenderServiceForm.vue';
+import ServiceControl from '@/components/Maintenance/ServiceControl.vue';
 
 @Component({
 	components: {
 		MenderServiceForm,
+		ServiceControl,
 	},
 	beforeRouteEnter(_to: Route, from: Route, next: NavigationGuardNext): void {
 		next((vm: Vue) => {
@@ -93,88 +53,5 @@ import {NavigationGuardNext, Route} from 'vue-router';
 /**
  * Mender control component
  */
-export default class MenderControl extends Vue {
-
-	/**
-	 * @constant {string} serviceName Mender service name
-	 */
-	private serviceName = 'mender-client';
-
-	/**
-	 * @var {boolean} missing Indicates that Mender service is not installed
-	 */
-	private missing = false;
-
-	/**
-	 * @var {boolean} unsupported Indicates that Mender service is unsupported
-	 */
-	private unsupported = false;
-
-	/**
-	 * @var {ServiceStatus|null} service Mender service status object
-	 */
-	private service: ServiceStatus|null = null;
-
-	/**
-	 * Vue lifecycle hook mounted
-	 */
-	mounted(): void {
-		this.getStatus();
-	}
-
-	/**
-	 * Retrieves status of the Mender service
-	 */
-	private getStatus(): Promise<void> {
-		return ServiceService.getStatus(this.serviceName)
-			.then((status: ServiceStatus) => {
-				this.service = status;
-				this.unsupported = false;
-				this.$store.commit('spinner/HIDE');
-			})
-			.catch((error: AxiosError) => menderErrorToast(error, 'service.messages.statusFailed'));
-	}
-
-	/**
-	 * Enables the Mender service
-	 */
-	private enable(): void {
-		this.$store.commit('spinner/SHOW');
-		ServiceService.enable(this.serviceName)
-			.then(() => this.serviceSuccess('service.mender-client.messages.enable'))
-			.catch((error: AxiosError) => menderErrorToast(error, 'service.messages.enableFailed'));
-	}
-
-	/**
-	 * Disables the Mender service
-	 */
-	private disable(): void {
-		this.$store.commit('spinner/SHOW');
-		ServiceService.disable(this.serviceName)
-			.then(() => this.serviceSuccess('service.mender-client.messages.disable'))
-			.catch((error: AxiosError) => menderErrorToast(error, 'service.messages.disableFailed'));
-	}
-
-	/**
-	 * Restarts the Mender service
-	 */
-	private restart(): void {
-		this.$store.commit('spinner/SHOW');
-		ServiceService.restart(this.serviceName)
-			.then(() => this.serviceSuccess('service.mender-client.messages.restart'))
-			.catch((error: AxiosError) => menderErrorToast(error, 'service.messages.restartFailed'));
-	}
-
-	/**
-	 * Handles successful Service operations
-	 * @param {string} message Toast message
-	 */
-	private serviceSuccess(message: string): void {
-		this.getStatus().then(() => {
-			this.$toast.success(
-				this.$t(message).toString()
-			);
-		});
-	}
-}
+export default class MenderControl extends Vue {}
 </script>
--- src/pages/Maintenance/PixlaControl.vue
+++ src/pages/Maintenance/PixlaControl.vue
@@ -20,54 +20,17 @@ limitations under the License.
 			<h1 class='mr-auto'>
 				{{ $t('maintenance.pixla.title') }}
 			</h1>
-			<div v-if='!missing && !unsupported'>
-				<v-btn
-					color='primary'
-					href='https://www.pixla.online/'
-					target='_blank'
-				>
-					{{ $t('maintenance.pixla.dashboard') }}
-				</v-btn>
-			</div>
+			<v-btn
+				color='primary'
+				href='https://www.pixla.online/'
+				target='_blank'
+			>
+				{{ $t('maintenance.pixla.dashboard') }}
+			</v-btn>
 		</header>
 		<v-card>
 			<v-card-text>
-				<div>
-					<strong>{{ $t('service.status') }}</strong>
-				</div>
-				<div v-if='missing'>
-					{{ $t('service.states.missing') }}
-				</div>
-				<div v-if='unsupported'>
-					{{ $t('service.states.unsupported') }}
-				</div>
-				<div v-if='service !== null'>
-					{{ $t(`states.${service.enabled ? 'enabled' : 'disabled'}`) }},
-					{{ $t(`service.states.${service.active ? 'active' : 'inactive'}`) }}
-				</div>
-				<div v-if='service !== null'>
-					<v-btn
-						v-if='!service.enabled'
-						color='success'
-						small
-						@click='enable()'
-					>
-						{{ $t('service.actions.enable') }}
-					</v-btn> <v-btn
-						v-if='service.enabled'
-						color='error'
-						small
-						@click='disable()'
-					>
-						{{ $t('service.actions.disable') }}
-					</v-btn> <v-btn
-						color='primary'
-						small
-						@click='restart()'
-					>
-						{{ $t('service.actions.restart') }}
-					</v-btn>
-				</div>
+				<ServiceControl service-name='gwman-client' />
 				<PixlaForm />
 			</v-card-text>
 		</v-card>
@@ -76,20 +39,16 @@ limitations under the License.
 
 <script lang='ts'>
 import {Component, Vue} from 'vue-property-decorator';
-import PixlaForm from '@/components/Maintenance/PixlaForm.vue';
-
-import {pixlaErrorToast} from '@/helpers/errorToast';
-
-import ServiceService, {ServiceStatus} from '@/services/ServiceService';
-
-import {AxiosError} from 'axios';
 import {NavigationGuardNext, Route} from 'vue-router';
+import PixlaForm from '@/components/Maintenance/PixlaForm.vue';
+import ServiceControl from '@/components/Maintenance/ServiceControl.vue';
 
 @Component({
 	components: {
 		PixlaForm,
+		ServiceControl,
 	},
-	beforeRouteEnter(to: Route, from: Route, next: NavigationGuardNext): void {
+	beforeRouteEnter(_to: Route, from: Route, next: NavigationGuardNext): void {
 		next((vm: Vue) => {
 			if (!vm.$store.getters['features/isEnabled']('pixla')) {
 				vm.$toast.error(vm.$t('service.gwman-client.messages.disabled').toString());
@@ -105,97 +64,5 @@ import {NavigationGuardNext, Route} from 'vue-router';
 /**
  * Pixla maintenance service manager card
  */
-export default class PixlaControl extends Vue {
-	/**
-	 * @constant {string} serviceName Pixla service name
-	 */
-	private serviceName = 'gwman-client';
-
-	/**
-	 * @var {boolean} missing Indicates whether the pixla service is not installed
-	 */
-	private missing = false;
-
-	/**
-	 * @var {boolean} unsupported Indicates whether the pixla service is supported
-	 */
-	private unsupported = false;
-
-	/**
-	 * @var {ServiceStatus|null} service Pixla service status object
-	 */
-	private service: ServiceStatus|null = null;
-
-	/**
-	 * Retrieves service status
-	 */
-	mounted(): void {
-		this.getStatus();
-	}
-
-	/**
-	 * Enables the Pixla service
-	 */
-	private enable(): void {
-		this.$store.commit('spinner/SHOW');
-		ServiceService.enable(this.serviceName)
-			.then(() => {
-				this.getStatus();
-				this.$toast.success(
-					this.$t('service.gwman-client.messages.enable')
-						.toString()
-				);
-			})
-			.catch((error: AxiosError) => pixlaErrorToast(error, 'service.messages.enableFailed'));
-	}
-
-	/**
-	 * Disables the Pixla service
-	 */
-	private disable(): void {
-		this.$store.commit('spinner/SHOW');
-		ServiceService.disable(this.serviceName)
-			.then(() => {
-				this.getStatus();
-				this.$toast.success(
-					this.$t('service.gwman-client.messages.disable')
-						.toString()
-				);
-			})
-			.catch((error: AxiosError) => pixlaErrorToast(error, 'service.messages.disableFailed'));
-	}
-
-	/**
-	 * Restarts the Pixla service
-	 */
-	private restart(): void {
-		this.$store.commit('spinner/SHOW');
-		ServiceService.restart(this.serviceName)
-			.then(() => {
-				this.getStatus();
-				this.$toast.success(
-					this.$t('service.gwman-client.messages.restart')
-						.toString()
-				);
-			})
-			.catch((error: AxiosError) => pixlaErrorToast(error, 'service.messages.restartFailed'));
-	}
-
-	/**
-	 * Retrieves status of the Pixla service
-	 */
-	private getStatus(): void {
-		if (!this.$store.getters['spinner/isEnabled']) {
-			this.$store.commit('spinner/SHOW');
-		}
-		ServiceService.getStatus(this.serviceName)
-			.then((status: ServiceStatus) => {
-				this.service = status;
-				this.unsupported = false;
-				this.$store.commit('spinner/HIDE');
-			})
-			.catch((error: AxiosError) => pixlaErrorToast(error, 'service.messages.statusFailed'));
-	}
-
-}
+export default class PixlaControl extends Vue {}
 </script>
