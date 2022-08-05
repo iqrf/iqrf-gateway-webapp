22e890a7b8cdebe66fa057ad94a634cbe2ffb35a
Karel Hanák <karel.hanak@iqrf.org>
2022-08-05 15:35:44 +0200

    Sidebar: fixed feature filtering, added styles for nested menus

--- src/components/SidebarItems.vue
+++ src/components/SidebarItems.vue
@@ -1,84 +1,17 @@
 <template>
 	<v-list dense>
-		<span
-			v-for='(item, i) in filteredItems'
-			:key='i'
-		>
-			<v-list-item
-				v-if='item.children === undefined'
-				:to='item.to'
-				:href='item.href'
-				:target='item.target'
-				router
-				exact
-				active-class='font-italic font-weight-bold'
-			>
-				<v-list-item-icon v-if='item.icon !== undefined'>
-					<v-icon>{{ item.icon }}</v-icon>
-				</v-list-item-icon>
-				<v-list-item-content>
-					<v-list-item-title>{{ item.title }}</v-list-item-title>
-				</v-list-item-content>
-			</v-list-item>
-			<v-list-group
-				v-else
-				:group='item.to'
-				:prepend-icon='item.icon'
-			>
-				<template #activator>
-					<v-list-item-content>
-						<v-list-item-title>{{ item.title }}</v-list-item-title>
-					</v-list-item-content>
-				</template>
-				<span
-					v-for='(child, j) in item.children'
-					:key='j'
-				>
-					<v-list-item
-						v-if='child.children === undefined'
-						:to='child.to'
-						:href='child.href'
-						:target='child.target'
-						router
-						exact
-					>
-						<v-list-item-content>
-							<v-list-item-title>{{ child.title }}</v-list-item-title>
-						</v-list-item-content>
-					</v-list-item>
-					<v-list-group
-						v-else
-						:sub-group='true'
-						:group='child.to'
-						:prepend-icon='child.icon'
-					>
-						<template #activator>
-							<v-list-item-content>
-								<v-list-item-title>{{ child.title }}</v-list-item-title>
-							</v-list-item-content>
-						</template>
-						<v-list-item
-							v-for='(grandchild, k) in child.children'
-							:key='k'
-							:to='grandchild.to'
-							:href='grandchild.href'
-							:target='grandchild.target'
-							router
-							exact
-						>
-							<v-list-item-content>
-								<v-list-item-title>{{ grandchild.title }}</v-list-item-title>
-							</v-list-item-content>
-						</v-list-item>
-					</v-list-group>
-				</span>
-			</v-list-group>
-		</span>
+		<template v-for='(navItem, idx) in filteredItems'>
+			<NavGroup v-if='navItem.children !== undefined && navItem.children.length > 0' :key='idx' :item='navItem' />
+			<NavItem v-else :key='idx' :item='navItem' />
+		</template>
 	</v-list>
 </template>
 
 <script lang='ts'>
 import {Component, Prop, Vue} from 'vue-property-decorator';
+import NavGroup from './NavGroup.vue';
+import NavItem from './NavItem.vue';
+
 import {LinkTarget} from '@/helpers/DisambiguationHelper';
 import {UserRoleIndex} from '@/services/AuthenticationService';
 
@@ -96,7 +29,12 @@ export interface NavigationItem {
 /**
  * Sidebar items
  */
-@Component({})
+@Component({
+	components: {
+		NavGroup,
+		NavItem,
+	},
+})
 export default class SidebarItems extends Vue {
 
 	/**
