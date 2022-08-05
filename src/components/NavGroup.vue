e27a06bcfdfbe87a948c8038e5e4c6b81af674b7
Karel Hanák <karel.hanak@iqrf.org>
2022-08-05 15:56:38 +0200

    Sidebar: added missing components headers and docs

--- src/components/NavGroup.vue
+++ src/components/NavGroup.vue
@@ -1,3 +1,19 @@
+<!--
+Copyright 2017-2021 IQRF Tech s.r.o.
+Copyright 2019-2021 MICRORISC s.r.o.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software,
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
 <template>
 	<v-list-group
 		v-if='!subGroup'
@@ -71,6 +87,9 @@ import NavListGroup from '@/components/NavListGroup';
 
 import {NavigationItem} from './SidebarItems.vue';
 
+/**
+ * Navigation menu item group component
+ */
 @Component({
 	components: {
 		NavListGroup,
@@ -78,8 +97,14 @@ import {NavigationItem} from './SidebarItems.vue';
 	},
 })
 export default class NavGroup extends Vue {
+	/**
+	 * @property {NavigationItem} item Navigation item
+	 */
 	@Prop({required: true}) item!: NavigationItem;
 
+	/**
+	 * @property {boolean} subGroup Indicates whether item is a top level group or not
+	 */
 	@Prop({required: false, type: Boolean, default: false}) subGroup!: boolean;
 }
 </script>
