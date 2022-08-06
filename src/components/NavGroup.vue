c45c25862e54c3b839990d41117e4eaa64d3a08c
Karel Hanák <karel.hanak@iqrf.org>
2022-08-06 12:31:59 +0200

    Nav menu: removed custom list group component, updated vuetify default
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- src/components/NavGroup.vue
+++ src/components/NavGroup.vue
@@ -16,14 +16,10 @@ limitations under the License.
 -->
 <template>
 	<v-list-group
-		v-if='!subGroup'
+		:sub-group='subGroup'
 		:group='item.to'
+		:prepend-icon='subGroup ? "" : item.icon'
 	>
-		<template #prependIcon>
-			<v-icon v-if='item.icon !== undefined'>
-				{{ item.icon }}
-			</v-icon>
-		</template>
 		<template v-if='subGroup' #appendIcon>
 			<v-icon>mdi-chevron-down</v-icon>
 		</template>
@@ -44,46 +40,11 @@ limitations under the License.
 			/>
 		</template>
 	</v-list-group>
-	<NavListGroup
-		v-else
-		:sub-group='subGroup'
-		:to='item.to'
-		:href='item.href'
-		:target='item.href'
-		router
-		exact
-	>
-		<template #prependIcon>
-			<v-icon v-if='item.icon !== undefined'>
-				{{ item.icon }}
-			</v-icon>
-		</template>
-		<template v-if='subGroup' #appendIcon>
-			<v-icon>mdi-chevron-down</v-icon>
-		</template>
-		<template #activator>
-			<v-list-item-title>{{ item.title }}</v-list-item-title>
-		</template>
-		<template v-for='(navItem, idx) in item.children'>
-			<NavGroup
-				v-if='navItem.children !== undefined && navItem.children.length > 0'
-				:key='idx'
-				:item='navItem'
-				:sub-group='true'
-			/>
-			<NavItem
-				v-else
-				:key='idx'
-				:item='navItem'
-			/>
-		</template>
-	</NavListGroup>
 </template>
 
 <script lang='ts'>
 import {Component, Prop, Vue} from 'vue-property-decorator';
 import NavItem from './NavItem.vue';
-import NavListGroup from '@/components/NavListGroup';
 
 import {NavigationItem} from './SidebarItems.vue';
 
@@ -92,7 +53,6 @@ import {NavigationItem} from './SidebarItems.vue';
  */
 @Component({
 	components: {
-		NavListGroup,
 		NavItem,
 	},
 })
