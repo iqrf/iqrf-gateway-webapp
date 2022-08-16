4ddfc3a6e78c99c1c6cce9bd0fa7b21f66fa9b64
Roman Ondráček <ondracek.roman@centrum.cz>
2022-08-16 18:49:03 +0200

    Move code from main.ts into separate plugin files
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- src/store/modules/daemonClient.module.ts
+++ src/store/modules/daemonClient.module.ts
@@ -16,7 +16,7 @@
  */
 import store from '@/store';
 import Vue from 'vue';
-import i18n from '@/i18n';
+import i18n from '@/plugins/i18n';
 
 import {v4 as uuidv4} from 'uuid';
 import {ActionTree, GetterTree, MutationTree} from 'vuex';
