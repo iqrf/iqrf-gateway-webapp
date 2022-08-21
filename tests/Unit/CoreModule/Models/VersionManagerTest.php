809be41ed6465eb1edfb2741cb553a2f7d2ec514
Karel Hanák <karel.hanak@iqrf.org>
2022-08-21 14:52:21 +0200

    Release version 2.4.11

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.11-alpha';
+	private const CURRENT_VERSION = '2.4.11';
 
 	/**
 	 * Current stable version of the webapp
