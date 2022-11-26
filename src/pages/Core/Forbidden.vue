463fa05558d7a3827c4db119915953e14d4e273b
Roman Ondráček <ondracek.roman@centrum.cz>
2022-11-26 18:32:43 +0100

    Core: add page for error 403
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

---
+++ src/pages/Core/Forbidden.vue
@@ -0,0 +1,46 @@
+<!--
+Copyright 2017-2021 IQRF Tech s.r.o.
+Copyright 2019-2021 MICRORISC s.r.o.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software,
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+<template>
+	<div>
+		<h1>{{ $t('core.error.403.title') }}</h1>
+		<CCard>
+			<CCardHeader>{{ $t('core.error.403.header') }}</CCardHeader>
+			<CCardBody>{{ $t('core.error.403.message') }}</CCardBody>
+		</CCard>
+	</div>
+</template>
+
+<script lang='ts'>
+import {Component, Vue} from 'vue-property-decorator';
+import {CCard, CCardBody, CCardHeader} from '@coreui/vue/src';
+
+@Component({
+	components: {
+		CCard,
+		CCardBody,
+		CCardHeader,
+	},
+	metaInfo: {
+		title: 'core.error.403.title',
+	},
+})
+
+/**
+ * Error 403 page component
+ */
+export default class Forbidden extends Vue {}
+</script>
