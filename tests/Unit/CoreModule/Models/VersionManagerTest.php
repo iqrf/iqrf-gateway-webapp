ef32d4315a4441156bee3e2312d931756ef72b39
Roman Ondráček <ondracek.roman@centrum.cz>
2022-12-07 13:34:24 +0100

    Prepare for version 2.4.14-alpha
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.13';
+	private const CURRENT_VERSION = '2.4.14-alpha';
 
 	/**
 	 * Current stable version of the webapp
--- tests/Unit/GatewayModule/Models/VersionManagerTest.php
+++ tests/Unit/GatewayModule/Models/VersionManagerTest.php
@@ -129,7 +129,7 @@ final class VersionManagerTest extends WebSocketTestCase {
 	/**
 	 * IQRF Gateway Webapp's version
 	 */
-	private const WEBAPP_VERSION = 'v2.4.13';
+	private const WEBAPP_VERSION = 'v2.4.14-alpha';
 
 	/**
 	 * Sets up the test environment
