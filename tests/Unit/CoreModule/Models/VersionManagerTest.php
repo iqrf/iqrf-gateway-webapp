15ea2e1117c0ac3281bccea176e4d2b65e05a088
Roman Ondráček <ondracek.roman@centrum.cz>
2022-12-07 13:26:53 +0100

    Release version 2.4.13
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.13-alpha';
+	private const CURRENT_VERSION = '2.4.13';
 
 	/**
 	 * Current stable version of the webapp
--- tests/Unit/GatewayModule/Models/VersionManagerTest.php
+++ tests/Unit/GatewayModule/Models/VersionManagerTest.php
@@ -129,7 +129,7 @@ final class VersionManagerTest extends WebSocketTestCase {
 	/**
 	 * IQRF Gateway Webapp's version
 	 */
-	private const WEBAPP_VERSION = 'v2.4.13-alpha';
+	private const WEBAPP_VERSION = 'v2.4.13';
 
 	/**
 	 * Sets up the test environment
