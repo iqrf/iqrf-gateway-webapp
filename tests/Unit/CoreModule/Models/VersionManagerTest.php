0f9383c4617cabe4dd677dddc76824c31a34c77c
Karel Hanák <karel.hanak@iqrf.org>
2023-01-25 12:27:01 +0100

    Prepare for version 2.5.0-rc5
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.0-rc4';
+	private const CURRENT_VERSION = '2.5.0-rc5';
 
 	/**
 	 * @var string Current stable version of the webapp
