ef8907d0a34bed3cf8a9ada41bbc2d7c436e6cf1
Karel Hanák <karel.hanak@iqrf.org>
2023-01-30 13:57:54 +0100

    Prepare for version 2.5.0-rc6

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.0-rc5';
+	private const CURRENT_VERSION = '2.5.0-rc6';
 
 	/**
 	 * @var string Current stable version of the webapp
