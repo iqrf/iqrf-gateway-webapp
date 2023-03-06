bc880eea5c8d088ce42406faf2d9550cfcbcf34e
Roman Ondráček <ondracek.roman@centrum.cz>
2023-03-06 10:57:43 +0100

    Prepare for version 3.0.0-alpha
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.0';
+	private const CURRENT_VERSION = '3.0.0-alpha';
 
 	/**
 	 * @var string Current stable version of the webapp
