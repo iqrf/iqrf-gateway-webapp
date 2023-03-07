ad703f09570243b11f475b7782846f5b9c3733fb
Roman Ondráček <ondracek.roman@centrum.cz>
2023-03-07 22:17:35 +0100

    Release version 2.5.2
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.2-alpha';
+	private const CURRENT_VERSION = '2.5.2';
 
 	/**
 	 * @var string Current stable version of the webapp
