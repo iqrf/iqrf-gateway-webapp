325b67c74787215cda3f67a501b6646fc730da3f
Roman Ondráček <ondracek.roman@centrum.cz>
2023-09-18 11:13:47 +0200

    Gateway: add missing return type hint in diagnostic manager
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- app/GatewayModule/Models/DiagnosticsManager.php
+++ app/GatewayModule/Models/DiagnosticsManager.php
@@ -287,7 +287,7 @@ class DiagnosticsManager {
 	/**
 	 * Adds tuptime (startup/shutdown/downtime) info
 	 */
-	public function addTuptime() {
+	public function addTuptime(): void {
 		if ($this->commandManager->commandExist('tuptime')) {
 			$output = $this->commandManager->run('tuptime -kpt')->getStdout();
 			$this->zipManager->addFileFromText('tuptime.txt', $output);
