7a3178a923432438b72c7c59cba5e9a1a2043a73
Karel Hanák <karel.hanak@iqrf.org>
2023-10-19 09:43:59 +0200

    Release version 2.5.12
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.12-alpha';
+	private const CURRENT_VERSION = '2.5.12';
 
 	/**
 	 * @var string Current stable version of the webapp
