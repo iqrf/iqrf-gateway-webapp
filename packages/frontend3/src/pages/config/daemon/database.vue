3782fc9859303d827715b1bda5617f840876e270
Karel Hanák <karel.hanak@iqrf.org>
2023-11-05 11:03:23 +0100

    frontend3: added iqrfdb configuration
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

---
+++ packages/frontend3/src/pages/config/daemon/database.vue
@@ -0,0 +1,16 @@
+<template>
+	<Head>
+		<title>{{ $t('pages.configuration.daemon.db.title') }}</title>
+	</Head>
+	<IqrfDbConfig />
+</template>
+
+<route lang='yaml'>
+name: DatabaseConfiguration
+</route>
+
+<script lang='ts' setup>
+import { Head } from '@unhead/vue/components';
+
+import IqrfDbConfig from '@/components/config/daemon/database/IqrfDbConfig.vue';
+</script>
