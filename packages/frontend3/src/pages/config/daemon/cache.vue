5332744fcbbcf38581119ebcaf855bc0a292b028
Karel Hanák <karel.hanak@iqrf.org>
2023-11-05 10:10:44 +0100

    frontend3: added js cache configuration
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

---
+++ packages/frontend3/src/pages/config/daemon/cache.vue
@@ -0,0 +1,16 @@
+<template>
+	<Head>
+		<title>{{ $t('pages.configuration.daemon.cache.title') }}</title>
+	</Head>
+	<JsCacheConfig />
+</template>
+
+<route lang='yaml'>
+name: CacheConfiguration
+</route>
+
+<script lang='ts' setup>
+import { Head } from '@unhead/vue/components';
+
+import JsCacheConfig from '@/components/config/daemon/cache/JsCacheConfig.vue';
+</script>
