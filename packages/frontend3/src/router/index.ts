b2bed6a21681d80f3857e9ac892ac270451b1dc6
Karel Hanák <karel.hanak@iqrf.org>
2023-11-08 07:04:33 +0100

    frontend: set global background styles, removed features from router navigation guard
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

--- packages/frontend3/src/router/index.ts
+++ packages/frontend3/src/router/index.ts
@@ -3,7 +3,6 @@ import { setupLayouts } from 'virtual:generated-layouts';
 import generatedRoutes from 'virtual:generated-pages';
 import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized, type RouteRecordRaw } from 'vue-router';
 
-import { useFeatureStore } from '@/store/features';
 import { useUserStore } from '@/store/user';
 
 const routes: RouteRecordRaw[] = [
@@ -17,7 +16,6 @@ const router = createRouter({
 
 router.beforeEach((to: RouteLocationNormalized, _from: RouteLocationNormalized, next: NavigationGuardNext) => {
 	const userStore = useUserStore();
-	const featureStore = useFeatureStore();
 	const requiresAuth: boolean = (to.meta.requiresAuth ?? true) === true;
 	const install: boolean = (to.meta.installWizard ?? false) === true;
 	if (!install && requiresAuth && !userStore.isLoggedIn) {
@@ -29,10 +27,6 @@ router.beforeEach((to: RouteLocationNormalized, _from: RouteLocationNormalized, 
 	}
 	if(to.name === 'SignIn' && userStore.isLoggedIn) {
 		return next((to.query.redirect as string|undefined) ?? '/');
-	}
-	const feature: string|undefined = (to.meta.feature as string) ?? undefined;
-	if (feature && feature in Feature && !featureStore.isEnabled(feature as Feature)) {
-		return next('/');
 	}
 	next();
 });
