638787681ff4a6de0de15373850cea1b20abd331
Karel Hanák <karel.hanak@iqrf.org>
2024-01-18 09:03:01 +0100

    Prepare for version 2.4.19-alpha

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -47,7 +47,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.4.18';
+	private const CURRENT_VERSION = '2.4.19-alpha';
 
 	/**
 	 * Current stable version of the webapp
--- tests/Unit/GatewayModule/Models/VersionManagerTest.php
+++ tests/Unit/GatewayModule/Models/VersionManagerTest.php
@@ -129,7 +129,7 @@ final class VersionManagerTest extends WebSocketTestCase {
 	/**
 	 * IQRF Gateway Webapp's version
 	 */
-	private const WEBAPP_VERSION = 'v2.4.18';
+	private const WEBAPP_VERSION = 'v2.4.19-alpha';
 
 	/**
 	 * Sets up the test environment
