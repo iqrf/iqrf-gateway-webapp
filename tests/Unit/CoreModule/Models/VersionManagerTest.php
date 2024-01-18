39c4e59715764fe0dfeab974dc662636eac66a6e
Karel Hanák <karel.hanak@iqrf.org>
2024-01-18 09:42:33 +0100

    Prepare for version 2.5.17-alpha

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.16';
+	private const CURRENT_VERSION = '2.5.17-alpha';
 
 	/**
 	 * @var string Current stable version of the webapp
