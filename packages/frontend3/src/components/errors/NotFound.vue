d82f8867fc0337c7fbf04ab85acf7ab477c37461
Roman Ondráček <mail@romanondracek.cz>
2024-02-22 15:37:04 +0100

    Frontend: improve access control
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

---
+++ packages/frontend3/src/components/errors/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+	<Head>
+		<title>{{ $t('components.errors.404.title') }}</title>
+	</Head>
+	<Card>
+		<template #title>
+			{{ $t('components.errors.404.header') }} - {{ $t('components.errors.404.title') }}
+		</template>
+		<p>{{ $t('components.errors.404.text') }}</p>
+	</Card>
+</template>
+
+<script setup lang='ts'>
+import { Head } from '@unhead/vue/components';
+
+import Card from '@/components/Card.vue';
+</script>
