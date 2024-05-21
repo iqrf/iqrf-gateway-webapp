4dc8c3c2c7631e644c7cc25cae03df9cad013778
Karel Hanák <karel.hanak@iqrf.org>
2024-05-21 13:23:12 +0200

    daemon-utils: added iqmesh network ping result item interface
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

---
+++ packages/daemon-utils/src/types/iqmesh/Network.ts
@@ -0,0 +1,9 @@
+/**
+ * IQMESH Network ping result interface
+ */
+export interface PingResultItem {
+	/// Address
+	address: number;
+	/// Online status
+	result: boolean;
+}
--- packages/daemon-utils/src/types/iqmesh/index.ts
+++ packages/daemon-utils/src/types/iqmesh/index.ts
@@ -3,5 +3,6 @@ export * from './Bonding';
 export * from './DpaParameters';
 export * from './Enumeration';
 export * from './Maintenance';
+export * from './Network';
 export * from './OtaUpload';
 export * from './TrConfiguration';
