e7eeeed6bc9e3e84ed2c113b22c35cdbe2b5df27
Roman Ondráček <mail@romanondracek.cz>
2024-07-10 20:23:55 +0200

    Docker: add build script for testing images
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

---
+++ docker/tests/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright 2017-2024 IQRF Tech s.r.o.
+# Copyright 2019-2024 MICRORISC s.r.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+VERSIONS=("bookworm")
+REPO="iqrftech/iqrf-gateway-webapp"
+
+for VERSION in "${VERSIONS[@]}"
+do
+   docker build --no-cache -f "${VERSION}.Dockerfile" -t "${REPO}:tests-${VERSION}" .
+   docker push "${REPO}:tests-${VERSION}"
+done
