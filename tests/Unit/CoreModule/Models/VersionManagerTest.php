69abb198d02eafc2957ee97a3abac820760903e4
Roman Ondráček <mail@romanondracek.cz>
2024-08-02 17:35:52 +0200

    Release version 2.5.21
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.20';
+	private const CURRENT_VERSION = '2.5.21';
 
 	/**
 	 * @var string Current stable version of the webapp
