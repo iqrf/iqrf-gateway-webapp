4e330e1857e1cd623e7d92578fcb31570a019c8f
Roman Ondráček <mail@romanondracek.cz>
2024-08-02 17:40:34 +0200

    Prepare for version v2.5.22-alpha
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.5.21';
+	private const CURRENT_VERSION = '2.5.22-alpha';
 
 	/**
 	 * @var string Current stable version of the webapp
