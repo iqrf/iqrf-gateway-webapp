db2d8f1e4a6ca2985066027e1b488346e06f5da3
Karel Hanák <karel.hanak@iqrf.org>
2024-10-21 12:26:51 +0200

    Release version 2.6.0-rc1

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.6.0-alpha';
+	private const CURRENT_VERSION = '2.6.0-rc1';
 
 	/**
 	 * @var string Current stable version of the webapp
