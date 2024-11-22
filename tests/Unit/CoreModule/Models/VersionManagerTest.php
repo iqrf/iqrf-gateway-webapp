695a31b3ac722373d8ed0f7f58a75da9d5ef8302
Karel Hanák <karel.hanak@iqrf.org>
2024-11-22 10:54:01 -0500

    Prepare for version 2.6.2-alpha

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.6.1';
+	private const CURRENT_VERSION = '2.6.2-alpha';
 
 	/**
 	 * @var string Current stable version of the webapp
