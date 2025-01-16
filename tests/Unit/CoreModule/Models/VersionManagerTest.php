17e4943ca2a893bb85f031768d9cfcaded8f8fed
Karel Hanák <karel.hanak@iqrf.org>
2025-01-16 13:44:58 +0100

    Release version 2.6.3

--- tests/Unit/CoreModule/Models/VersionManagerTest.php
+++ tests/Unit/CoreModule/Models/VersionManagerTest.php
@@ -48,7 +48,7 @@ final class VersionManagerTest extends CommandTestCase {
 	/**
 	 * @var string Current version of the webapp
 	 */
-	private const CURRENT_VERSION = '2.6.3-alpha';
+	private const CURRENT_VERSION = '2.6.3';
 
 	/**
 	 * @var string Current stable version of the webapp
