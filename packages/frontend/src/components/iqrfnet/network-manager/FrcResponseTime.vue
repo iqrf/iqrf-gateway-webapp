e4d5f0d809efcb6ee1b92408eb94a0b704967a48
Karel Hanák <karel.hanak@iqrf.org>
2025-10-02 09:35:01 +0200

    frontend: added frc response time measuring
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

---
+++ packages/frontend/src/components/iqrfnet/network-manager/FrcResponseTime.vue
@@ -0,0 +1,147 @@
+<template>
+	<v-form
+		ref='form'
+		:disabled='componentState === ComponentState.Action'
+	>
+		<ICard flat tile>
+			<v-card-title>
+				{{ $t('components.iqrfnet.network-manager.frc-response-time.title') }}
+			</v-card-title>
+			<ISelectInput
+				v-model='command'
+				:label='$t("components.iqrfnet.network-manager.frc-response-time.command")'
+				:items='commandOptions'
+			/>
+			<template #actions>
+				<IActionBtn
+					:action='Action.Download'
+					:loading='componentState === ComponentState.Action'
+					:text='$t("components.iqrfnet.network-manager.frc-response-time.actions.get")'
+					@click='frcResponseTime()'
+				/>
+				<FrcResponseTimeResultDialog
+					ref='resultModal'
+					:result='result'
+				/>
+			</template>
+		</ICard>
+	</v-form>
+</template>
+
+<script lang='ts' setup>
+import { IqmeshServiceMessages } from '@iqrf/iqrf-gateway-daemon-utils/enums';
+import { FrcCommand } from '@iqrf/iqrf-gateway-daemon-utils/enums/embed';
+import { MaintenanceService } from '@iqrf/iqrf-gateway-daemon-utils/services/iqmesh';
+import { DaemonApiResponse } from '@iqrf/iqrf-gateway-daemon-utils/types';
+import { IqmeshFrcResponseTimeParams } from '@iqrf/iqrf-gateway-daemon-utils/types/iqmesh';
+import { DaemonMessageOptions } from '@iqrf/iqrf-gateway-daemon-utils/utils';
+import { Action, ComponentState, IActionBtn, ICard, ISelectInput } from '@iqrf/iqrf-vue-ui';
+import { onBeforeUnmount, ref, Ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { toast } from 'vue3-toastify';
+
+import FrcResponseTimeResultDialog from '@/components/iqrfnet/network-manager/FrcResponseTimeResultDialog.vue';
+import { useDaemonStore } from '@/store/daemonSocket';
+import { FrcResponseTimeResult } from '@/types/DaemonApi/Iqmesh';
+
+const componentState: Ref<ComponentState> = ref(ComponentState.Idle);
+const i18n = useI18n();
+const daemonStore = useDaemonStore();
+const command: Ref<FrcCommand> = ref(FrcCommand.Iqrf1Byte);
+const commandOptions = [
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf2bits'),
+		value: FrcCommand.Iqrf2Bits,
+	},
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf1byte'),
+		value: FrcCommand.Iqrf1Byte,
+	},
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf2byte'),
+		value: FrcCommand.Iqrf2Bytes,
+	},
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf4byte'),
+		value: FrcCommand.Iqrf4Bytes,
+	},
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user2bits'),
+		value: FrcCommand.User2Bits,
+	},
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user1byte'),
+		value: FrcCommand.User1Byte,
+	},
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user2byte'),
+		value: FrcCommand.User2Bytes,
+	},
+	{
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user4byte'),
+		value: FrcCommand.User4Bytes,
+	},
+];
+const resultModal: Ref<typeof FrcResponseTimeResultDialog | null> = ref(null);
+const result: Ref<FrcResponseTimeResult | null> = ref(null);
+const msgId: Ref<string | null> = ref(null);
+
+daemonStore.$onAction(({ name, after }) => {
+	if (name === 'onMessage') {
+		after((rsp: DaemonApiResponse) => {
+			if (rsp.data.msgId !== msgId.value) {
+				return;
+			}
+			daemonStore.removeMessage(msgId.value);
+			componentState.value = ComponentState.Idle;
+			if (rsp.mType === IqmeshServiceMessages.MaintenanceFrcResponseTime) {
+				handleFrcResponseTime(rsp);
+			}
+		});
+	}
+});
+
+async function frcResponseTime(): Promise<void> {
+	componentState.value = ComponentState.Action;
+	result.value = null;
+	const opts = new DaemonMessageOptions(
+		null,
+		400_000,
+		i18n.t('components.iqrfnet.network-manager.frc-response-time.messages.get.timeout'),
+		() => {
+			componentState.value = ComponentState.Idle;
+			msgId.value = null;
+		},
+	);
+	const params: IqmeshFrcResponseTimeParams = {
+		command: command.value,
+	};
+	msgId.value = await daemonStore.sendMessage(
+		MaintenanceService.frcResponseTime(
+			{ repeat: 1, returnVerbose: true },
+			params,
+			opts,
+		),
+	);
+}
+
+function handleFrcResponseTime(rsp: DaemonApiResponse): void {
+	if (rsp.data.status === 0) {
+		result.value = rsp.data.rsp as FrcResponseTimeResult;
+		resultModal.value?.open();
+	} else if (rsp.data.status === 1_003) {
+		toast.info(
+			i18n.t('common.messages.noNodes'),
+		);
+	} else {
+		toast.error(
+			i18n.t('components.iqrfnet.network-manager.frc-response-time.messages.get.failed'),
+		);
+	}
+}
+
+onBeforeUnmount(() => {
+	daemonStore.removeMessage(msgId.value);
+});
+
+</script>
---
+++ packages/frontend/src/components/iqrfnet/network-manager/FrcResponseTimeResultDialog.vue
@@ -0,0 +1,162 @@
+<template>
+	<IModalWindow
+		v-model='show'
+		persistent
+	>
+		<ICard>
+			<template #title>
+				{{ $t('components.iqrfnet.network-manager.frc-response-time.modal.title') }}
+			</template>
+			<v-table
+				v-if='result'
+				density='compact'
+			>
+				<tbody>
+					<tr>
+						<th>{{ $t('components.iqrfnet.network-manager.frc-response-time.command') }}</th>
+						<td class='text-end'>
+							{{ commandLabel }}
+						</td>
+					</tr>
+					<tr>
+						<th>{{ $t('components.iqrfnet.network-manager.frc-response-time.modal.inaccessibleNodes') }}</th>
+						<td class='text-end'>
+							{{ result.inaccessibleNodes }}
+						</td>
+					</tr>
+					<tr>
+						<th>{{ $t('components.iqrfnet.network-manager.frc-response-time.modal.unhandledNodes') }}</th>
+						<td class='text-end'>
+							{{ result.unhandledNodes }}
+						</td>
+					</tr>
+					<tr>
+						<th>{{ $t('components.iqrfnet.network-manager.frc-response-time.modal.current') }}</th>
+						<td class='text-end'>
+							{{ result.currentResponseTime }}
+						</td>
+					</tr>
+					<tr>
+						<th>{{ $t('components.iqrfnet.network-manager.frc-response-time.modal.recommended') }}</th>
+						<td class='text-end'>
+							{{ result.recommendedResponseTime }}
+						</td>
+					</tr>
+				</tbody>
+			</v-table>
+			<v-divider class='my-2' />
+			<IDataTable
+				v-if='result'
+				:headers='headers'
+				:items='result.nodes'
+				:hover='true'
+				:dense='true'
+				disable-column-filtering
+				disable-search
+			>
+				<template #item.responded='{ item }'>
+					<IBooleanIcon
+						:value='item.responded'
+					/>
+				</template>
+				<template #item.handled='{ item }'>
+					<IBooleanIcon
+						v-if='item.responded'
+						:value='item.handled'
+					/>
+					<span v-else>
+						{{ $t('components.iqrfnet.network-manager.frc-response-time.modal.notAvailable') }}
+					</span>
+				</template>
+				<template #item.responseTime='{ item }'>
+					<span v-if='item.responded && item.handled'>{{ item.responseTime }}</span>
+					<span v-else>{{ $t('components.iqrfnet.network-manager.frc-response-time.modal.notAvailable') }}</span>
+				</template>
+			</IDataTable>
+			<template #actions>
+				<IActionBtn
+					:action='Action.Close'
+					:disabled='componentState === ComponentState.Action'
+					@click='close()'
+				/>
+			</template>
+		</ICard>
+	</IModalWindow>
+</template>
+
+<script lang='ts' setup>
+import { FrcCommand } from '@iqrf/iqrf-gateway-daemon-utils/enums/embed';
+import { Action, ComponentState, IActionBtn, IBooleanIcon, ICard, IDataTable, IModalWindow } from '@iqrf/iqrf-vue-ui';
+import { computed, PropType, ref, Ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import { FrcResponseTimeResult } from '@/types/DaemonApi/Iqmesh';
+
+const componentState: Ref<ComponentState> = ref(ComponentState.Idle);
+const componentProps = defineProps({
+	result: {
+		type: [Object, null] as PropType<FrcResponseTimeResult | null>,
+		required: false,
+		default: null,
+	},
+});
+defineExpose({
+	open,
+});
+const i18n = useI18n();
+const show: Ref<boolean> = ref(false);
+const commandLabel = computed(() => {
+	switch (componentProps.result?.command) {
+		case FrcCommand.Iqrf2Bits:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf2bits');
+		case FrcCommand.Iqrf1Byte:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf1byte');
+		case FrcCommand.Iqrf2Bytes:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf2byte');
+		case FrcCommand.Iqrf4Bytes:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.iqrf4byte');
+		case FrcCommand.User2Bits:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user2bits');
+		case FrcCommand.User1Byte:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user1byte');
+		case FrcCommand.User2Bytes:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user2byte');
+		default:
+			return i18n.t('components.iqrfnet.network-manager.frc-response-time.commands.user4byte');
+	}
+});
+
+const headers = [
+	{
+		key: 'deviceAddr',
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.modal.table.address'),
+		width: '25%',
+	},
+	{
+		key: 'responded',
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.modal.table.responded'),
+		filterable: false,
+		width: '25%',
+	},
+	{
+		key: 'handled',
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.modal.table.responded'),
+		filterable: false,
+		width: '25%',
+	},
+	{
+		key: 'responseTime',
+		title: i18n.t('components.iqrfnet.network-manager.frc-response-time.modal.table.responseTime'),
+		width: '25%',
+	},
+];
+
+function open(): void {
+	show.value = true;
+}
+
+function close(): void {
+	show.value = false;
+}
+
+</script>
