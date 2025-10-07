eddfc9a1b5253f324ee5b36b434b5ac8bfcff6c8
Karel Hanák <karel.hanak@iqrf.org>
2025-10-07 11:44:40 +0200

    frontend: finished trconfig implementation
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- packages/frontend/src/components/iqrfnet/tr-config/TrConfigDpaForm.vue
+++ packages/frontend/src/components/iqrfnet/tr-config/TrConfigDpaForm.vue
@@ -23,58 +23,60 @@ limitations under the License.
 					{{ $t('components.iqrfnet.common.embedPeripherals') }}
 				</legend>
 				<v-checkbox
-					v-model='config.embPers!.eeprom'
+					v-model='config.embPers.eeprom'
 					:label='$t("components.iqrfnet.common.peripherals.eeprom")'
 					density='compact'
 					hide-details
 				/>
 				<v-checkbox
-					v-model='config.embPers!.eeeprom'
+					v-model='config.embPers.eeeprom'
 					:label='$t("components.iqrfnet.common.peripherals.eeeprom")'
 					density='compact'
 					hide-details
 				/>
 				<v-checkbox
-					v-model='config.embPers!.ram'
+					v-model='config.embPers.ram'
 					:label='$t("components.iqrfnet.common.peripherals.ram")'
 					density='compact'
 					hide-details
 				/>
 				<v-checkbox
-					v-model='config.embPers!.ledr'
+					v-model='config.embPers.ledr'
 					:label='$t("components.iqrfnet.common.peripherals.ledr")'
 					density='compact'
 					hide-details
 				/>
 				<v-checkbox
-					v-model='config.embPers!.ledg'
+					v-model='config.embPers.ledg'
 					:label='$t("components.iqrfnet.common.peripherals.ledg")'
 					density='compact'
 					hide-details
 				/>
 				<v-checkbox
-					v-model='config.embPers!.spi'
-					:label='$t("components.iqrfnet.common.peripherals.spi")'
+					v-model='config.embPers.spi'
+					:label='`${$t("components.iqrfnet.common.peripherals.spi")} (4, 7, 9)`'
 					density='compact'
 					hide-details
+					:disabled='config.deviceAddr === 0 || config.embPers.uart || compare(dpaVersion, "4.14", ">")'
 				/>
 				<v-checkbox
-					v-model='config.embPers!.io'
+					v-model='config.embPers.io'
 					:label='$t("components.iqrfnet.common.peripherals.io")'
 					density='compact'
 					hide-details
 				/>
 				<v-checkbox
-					v-model='config.embPers!.thermometer'
+					v-model='config.embPers.thermometer'
 					:label='$t("components.iqrfnet.common.peripherals.thermometer")'
 					density='compact'
 					hide-details
 				/>
 				<v-checkbox
-					v-model='config.embPers!.uart'
-					:label='$t("components.iqrfnet.common.peripherals.uart")'
+					v-model='config.embPers.uart'
+					:label='`${$t("components.iqrfnet.common.peripherals.uart")} (7, 9)`'
 					density='compact'
 					hide-details
+					:disabled='config.deviceAddr === 0 || config.embPers.spi'
 				/>
 			</v-col>
 			<v-divider :vertical='!display.mobile.value' />
@@ -87,12 +89,14 @@ limitations under the License.
 					:label='$t("components.iqrfnet.tr-config.dpa.other.customDpaHandler")'
 					density='compact'
 					hide-details
+					:disabled='!dpaHandlerDetected'
 				/>
 				<v-checkbox
 					v-model='config.dpaPeerToPeer'
-					:label='$t("components.iqrfnet.tr-config.dpa.other.dp2p")'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.other.dp2p")} (2, 7)`'
 					density='compact'
 					hide-details
+					:disabled='config.deviceAddr === 0 || compare(dpaVersion, "4.10", "<")'
 				/>
 				<v-checkbox
 					v-model='config.peerToPeer'
@@ -102,9 +106,10 @@ limitations under the License.
 				/>
 				<v-checkbox
 					v-model='config.localFrcReception'
-					:label='$t("components.iqrfnet.tr-config.dpa.other.localFrc")'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.other.localFrc")} (3, 7)`'
 					density='compact'
 					hide-details
+					:disabled='config.deviceAddr === 0 || compare(dpaVersion, "4.15", "<")'
 				/>
 				<v-checkbox
 					v-model='config.ioSetup'
@@ -114,75 +119,177 @@ limitations under the License.
 				/>
 				<v-checkbox
 					v-model='config.dpaAutoexec'
-					:label='$t("components.iqrfnet.tr-config.dpa.other.autoexec")'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.other.autoexec")} (4)`'
 					density='compact'
 					hide-details
+					:disabled='compare(dpaVersion, "4.14", ">")'
 				/>
 				<v-checkbox
 					v-model='config.routingOff'
 					:label='$t("components.iqrfnet.tr-config.dpa.other.routingOff")'
 					density='compact'
 					hide-details
+					:disabled='config.deviceAddr === 0'
 				/>
 				<v-checkbox
 					v-model='config.neverSleep'
-					:label='$t("components.iqrfnet.tr-config.dpa.other.neverSleep")'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.other.neverSleep")} (1, 7)`'
 					density='compact'
 					hide-details
+					:disabled='config.deviceAddr === 0 || compare(dpaVersion, "3.03", "<")'
 				/>
-				<SelectInput
-					v-model='config.uartBaudRate'
-					:label='$t("components.iqrfnet.tr-config.dpa.other.baudRate")'
+				<v-checkbox
+					v-model='config.nodeDpaInterface'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.other.nodeDpaInterface")} (2)`'
+					density='compact'
+					hide-details
+					:disabled='compare(dpaVersion, "4.00", ">=")'
+				/>
+				<ISelectInput
+					v-model='config.uartBaudrate'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.other.baudRate")} (8)`'
 					:items='baudRateOptions'
 				/>
 			</v-col>
 		</v-row>
 		<v-row>
 			<v-divider />
-			<v-col cols='6'>
+			<v-col>
 				<legend class='section-legend'>
 					{{ $t('components.iqrfnet.common.rf') }}
 				</legend>
-				<SelectInput
-					v-model='config.stdAndLpNetwork'
-					:label='$t("components.iqrfnet.tr-config.dpa.rf.networkType")'
-					:items='networkTypeOptions'
+				<v-alert
+					v-if='!config.stdAndLpNetwork'
+					class='mb-4'
+					type='warning'
+					variant='tonal'
+					:text='$t("components.iqrfnet.tr-config.dpa.notes.interoperability")'
 				/>
-				<NumberInput
+				<ISelectInput
+					v-model='config.stdAndLpNetwork'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.rf.networkType")} (6)`'
+					:items='networkTypeOptions'
+					:disabled='config.deviceAddr !== 0'
+				/>
+				<INumberInput
 					v-model='config.txPower'
 					:label='$t("components.iqrfnet.common.txPower")'
+					:rules='[
+						(v: number|null) => ValidationRules.required(v, $t("components.iqrfnet.common.validation.txPower.required")),
+						(v: number) => ValidationRules.integer(v, $t("components.iqrfnet.common.validation.txPower.integer")),
+						(v: number) => ValidationRules.between(v, 0, 7, $t("components.iqrfnet.common.validation.txPower.between")),
+					]'
+					:min='0'
+					:max='7'
+					required
 				/>
-				<NumberInput
+				<INumberInput
 					v-model='config.rxFilter'
 					:label='$t("components.iqrfnet.common.rxFilter")'
+					:rules='[
+						(v: number|null) => ValidationRules.required(v, $t("components.iqrfnet.common.validation.rxFilter.required")),
+						(v: number) => ValidationRules.integer(v, $t("components.iqrfnet.common.validation.rxFilter.integer")),
+						(v: number) => ValidationRules.between(v, 0, 64, $t("components.iqrfnet.common.validation.rxFilter.between")),
+					]'
+					:min='0'
+					:max='64'
+					required
 				/>
-				<NumberInput
+				<INumberInput
 					v-model='config.lpRxTimeout'
-					:label='$t("components.iqrfnet.tr-config.dpa.rf.lpRxTimeout")'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.rf.lpRxTimeout")} (7)`'
+					:rules='[
+						(v: number|null) => ValidationRules.required(v, $t("components.iqrfnet.common.validation.lpRxTimeout.required")),
+						(v: number) => ValidationRules.integer(v, $t("components.iqrfnet.common.validation.lpRxTimeout.integer")),
+						(v: number) => ValidationRules.between(v, 1, 255, $t("components.iqrfnet.common.validation.lpRxTimeout.between")),
+					]'
+					:min='1'
+					:max='255'
+					required
+					:disabled='config.deviceAddr === 0'
 				/>
-				<NumberInput
+				<INumberInput
 					v-model='config.rfAltDsmChannel'
-					:label='$t("components.iqrfnet.tr-config.dpa.rf.altDsmChannel")'
+					:label='`${$t("components.iqrfnet.tr-config.dpa.rf.altDsmChannel")} (5)`'
+					:rules='rfChannelRules'
+					:min='0'
+					:max='rfChannelMax'
+					required
 				/>
 			</v-col>
 			<v-divider :vertical='!display.mobile.value' />
+			<v-col>
+				<div>
+					<em>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.valid303Above') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.valid410Above') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.valid415Above') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.valid414Below') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.channelNote') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.coordinatorOnly') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.nodeOnly') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.uartOnly') }}<br>
+						{{ $t('components.iqrfnet.tr-config.dpa.notes.exclusive') }}
+					</em>
+				</div>
+			</v-col>
 		</v-row>
 	</ICard>
 </template>
 
 <script lang='ts' setup>
-import { IqmeshWriteTrConfParams } from '@iqrf/iqrf-gateway-daemon-utils/types';
-import { ICard } from '@iqrf/iqrf-vue-ui';
+import { ICard, INumberInput, ISelectInput, ValidationRules } from '@iqrf/iqrf-vue-ui';
+import { compare } from 'compare-versions';
 import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useDisplay } from 'vuetify';
 
-import NumberInput from '@/components/layout/form/NumberInput.vue';
-import SelectInput from '@/components/layout/form/SelectInput.vue';
+import { TrConfiguration } from '@/types/DaemonApi/Iqmesh';
 
 const i18n = useI18n();
 const display = useDisplay();
-const config = defineModel<IqmeshWriteTrConfParams>('config', { required: true });
+const config = defineModel<TrConfiguration & { deviceAddr: number }>('config', { required: true });
+defineProps({
+	dpaVersion: {
+		type: String,
+		required: true,
+	},
+	dpaHandlerDetected: {
+		type: Boolean,
+		required: true,
+	},
+});
+
+const rfChannelMax = computed(() => {
+	if (config.value.rfBand === '433') {
+		return 16;
+	} else if (config.value.rfBand === '868') {
+		return 67;
+	} else {
+		return 255;
+	}
+});
+
+const rfChannelRules = computed(() => {
+	const rules = [
+		(v: number|null) => ValidationRules.required(v, i18n.t('components.iqrfnet.common.validation.rfChannel.required')),
+		(v: number) => ValidationRules.integer(v, i18n.t('components.iqrfnet.common.validation.rfChannel.integer')),
+	];
+	if (config.value.rfBand === '433') {
+		rules.push(
+			(v: number) => ValidationRules.between(v, 0, 16, i18n.t('components.iqrfnet.common.validation.rfChannel.between433')),
+		);
+	} else if (config.value.rfBand === '868') {
+		rules.push(
+			(v: number) => ValidationRules.between(v, 0, 67, i18n.t('components.iqrfnet.common.validation.rfChannel.between868')),
+		);
+	} else {
+		rules.push(
+			(v: number) => ValidationRules.between(v, 0, 255, i18n.t('components.iqrfnet.common.validation.rfChannel.between916')),
+		);
+	}
+	return rules;
+});
 
 const baudRateOptions = computed(() => {
 	const items: number[] = [1_200, 2_400, 4_800, 9_600, 19_200, 38_400, 57_600, 115_200, 230_400];
